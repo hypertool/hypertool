b252e89a5908ac8410e3439ac6256195e551f703
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-14 23:38:49 +0530

    feat: added a logger middleware to the Express server

--- source/app.ts
+++ source/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import logger from "morgan";
 
 const initialize = async () => {
     const app = express();
+
+    if (process.env.NODE_ENV !== "production") {
+        app.use(logger("dev"));
+    }
+
     return app;
 };
 
