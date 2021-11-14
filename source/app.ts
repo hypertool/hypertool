382c2297c9375fbf27d8215d4595fb8c95dff4fe
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-14 23:41:07 +0530

    feat: added cors middleware to the Express server

--- source/app.ts
+++ source/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import logger from "morgan";
+import cors from "cors";
 
 const initialize = async () => {
     const app = express();
+    app.use(cors());
 
     if (process.env.NODE_ENV !== "production") {
         app.use(logger("dev"));
