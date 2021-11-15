b0533ab562ed48d2bdf4b2c139cbf7129445f2bf
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-15 22:57:56 +0530

    feat: created an empty login screen

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,9 @@
+import type { FunctionComponent, ReactElement } from "react";
+
+const Login: FunctionComponent = (): ReactElement => {
+    return <section>
+        Hello! This is the login screen.
+    </section>;
+};
+
+export default Login;
