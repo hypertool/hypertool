734d83a24d144aea192f493a36316119845402b0
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-15 22:05:25 +0530

    feat: attached the private GraphQL API path to the main router

--- source/app.ts
+++ source/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
+import * as graphql from "./graphql";
+
 const initialize = async () => {
     const app = express();
     app.use(cors());
@@ -9,6 +11,8 @@ const initialize = async () => {
     if (process.env.NODE_ENV !== "production") {
         app.use(logger("dev"));
     }
+
+    graphql.attachRoutes(app);
 
     return app;
 };
