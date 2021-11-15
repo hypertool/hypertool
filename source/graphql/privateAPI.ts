077e18b49dfb207b255bbbd65bf933ba65b6f182
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-15 10:34:07 +0530

    feat: created a GraphQL server for private APIs

---
+++ source/graphql/privateAPI.ts
@@ -0,0 +1,23 @@
+import { ApolloServer, gql } from "apollo-server-express";
+
+const typeDefs = gql``;
+
+const resolvers = {
+};
+
+const attachRoutes = async (app: any) => {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: (context) => ({
+            request: context.req
+        })
+    });
+    await server.start();
+    server.applyMiddleware({
+        app,
+        path: "/graphql/v1/private"
+    });
+};
+
+export { attachRoutes };
