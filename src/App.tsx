c3995eb3b8f9170ac60c3eb54533b00c6e2f6c27
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-15 23:03:51 +0530

    refactor: moved code from `App` component to `Test` page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,10 @@
-import { InputStream, CommonTokenStream } from "antlr4";
-import HTXLexer from './antlr-gen/HTXLexer';
-import HTXParser from './antlr-gen/HTXParser';
-import { Visitor } from "./htx";
-import LayoutRendererer from "./components/LayoutRenderer";
+import type { FunctionComponent, ReactElement } from "react";
 
-const compile = () => {
-  try {
-    const input2 = `<grid>
-    <grid_item xs=12>
-        <text id="hello">
-            Students
-        </text>
-    </grid_item>
+import { Test } from "./screens"; 
 
-    <grid_item xs=12 md=6>
-        <table id=1 data=null />
 
-        <!-- Never hide -->
-        <hidden when=false>
-            <!-- Never hide -->
-            <grid>
-                <grid_item xs=12>
-                    <text_field
-                        id="firstName"
-                        label="First Name"
-                        value=null />
-
-                    <text_field
-                        id="lastName"
-                        label="Last Name"
-                        value=null />
-
-                    <text_field
-                        id="age"
-                        type="number"
-                        label="Age"
-                        value=null />
-
-                    <button
-                        text="Update"
-                        onClick=null
-                        disabled=false />
-                </grid_item>
-            </grid>
-        </hidden>
-    </grid_item>
-</grid>`;
-
-  const input = `
-    <grid justifyContent="space-between" direction="row">
-      <grid_item xs=12 lg=2>
-        <box />
-      </grid_item>
-      <grid_item xs=12 lg=2>
-        <box />
-      </grid_item>
-      <grid_item lg=5>
-        <button_group>
-          <button>Profile</button>
-          <button>Preferences</button>
-          <button>Logout</button>
-        </button_group>
-      </grid_item>
-    </grid>`;
-
-    const chars = new InputStream(input);
-    const lexer = new HTXLexer(chars);
-    const tokens = new CommonTokenStream(lexer);
-    const parser = new HTXParser(tokens);
-    parser.buildParseTrees = true;
-    const tree = parser.compilationUnit();
-
-    
-    const visitor = new Visitor();
-    const result = visitor.visitCompilationUnit(tree);
-    console.log(result);
-
-    return result;
-  }
-  catch (error) {
-    console.log(error);
-  }
-};
-
-const App = () => {
-  const tree = compile();
-
-  return (
-    <LayoutRendererer layout={tree} />
-  );
+const App: FunctionComponent = (): ReactElement => {
+  return <Test />;
 }
 
 export default App;
