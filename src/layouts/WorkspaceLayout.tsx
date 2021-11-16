2f61e7625563498483679cc07d3e707dad635b70
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-16 12:25:50 +0530

    refactor: removed unnecessary import statement

--- src/layouts/WorkspaceLayout.tsx
+++ src/layouts/WorkspaceLayout.tsx
@@ -1,7 +1,6 @@
 import type { FunctionComponent, ReactElement } from "react";
 
 import { useState } from "react";
-import {  } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 import { MiniDrawer, PrimaryAppBar } from "../components";
