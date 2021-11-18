dbc0fcd7e9306670f1851b06d61f81ac71bd95e5
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-18 10:42:01 +0530

    feat: created a component for collecting resource details

---
+++ src/screens/new-app/ResourcesStep.tsx
@@ -0,0 +1,24 @@
+import type { FunctionComponent, ReactElement } from "react";
+
+import { styled } from "@mui/material/styles";
+
+import ResourcesTable from "./ResourcesTable";
+
+const Root = styled("section")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "flex-start",
+  paddingTop: theme.spacing(4),
+  paddingBottom: theme.spacing(4),
+}));
+
+const ResourcesStep: FunctionComponent = (): ReactElement => {
+  return (
+    <Root>
+      <ResourcesTable />
+    </Root>
+  );
+};
+
+export default ResourcesStep;
