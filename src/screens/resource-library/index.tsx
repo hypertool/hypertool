258dd8abef85e56f910e33a12bc92be95e44227a
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-18 15:34:14 +0530

    feat: added a toolbar in the Resource Library screen
    The toolbar includes a Search text field and a Create Resource button.

--- src/screens/resource-library/index.tsx
+++ src/screens/resource-library/index.tsx
@@ -1,7 +1,58 @@
 import type { FunctionComponent, ReactElement } from "react";
 
+import { styled } from "@mui/material/styles";
+import { AppBar, Toolbar, Button, Icon, Typography, TextField } from "@mui/material";
+
+const Root = styled("section")(({ theme }) => ({
+  width: "100%",
+}));
+
+const Title = styled(Typography)(({ theme }) => ({}));
+
+const WorkspaceToolbar = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+}));
+
+const ActionContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "flex-end",
+}));
+
+const Search = styled(TextField)(({ theme }) => ({
+  width: 256,
+  marginRight: theme.spacing(2)
+})) as any;
+
+const ActionIcon = styled(Icon)(({ theme }) => ({
+  marginRight: theme.spacing(1),
+}));
+
 const ResourceLibrary: FunctionComponent = (): ReactElement => {
-  return <section>View Resources</section>;
+  return (
+    <Root>
+      <AppBar position="static" elevation={1}>
+        <WorkspaceToolbar>
+          <Title>Resource Library</Title>
+          <ActionContainer>
+            <Search
+              label=""
+              placeholder="Search"
+              size="small"
+            />
+            <Button size="small">
+              <ActionIcon fontSize="small">add_circle</ActionIcon>
+              Create New
+            </Button>
+          </ActionContainer>
+        </WorkspaceToolbar>
+      </AppBar>
+    </Root>
+  );
 };
 
 export default ResourceLibrary;
