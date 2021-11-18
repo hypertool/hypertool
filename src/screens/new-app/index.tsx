30d5cf9cddc7a695805313ca6bfb93442a1d9797
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-18 10:47:14 +0530

    feat: added the New App Stepper to the New App screen

--- src/screens/new-app/index.tsx
+++ src/screens/new-app/index.tsx
@@ -1,7 +1,17 @@
 import { FunctionComponent, ReactElement } from "react";
 
+import { Container as MuiContainer } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+import Stepper from "./Stepper";
+
+const Container = styled(MuiContainer)(({ theme }) => ({
+  paddingTop: theme.spacing(4),
+  paddingBottom: theme.spacing(2),
+}));
+
 const NewApp: FunctionComponent = (): ReactElement => {
-  return <div>New App</div>;
+  return <Container><Stepper /></Container>;
 };
 
 export default NewApp;
