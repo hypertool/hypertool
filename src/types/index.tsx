7a2b4477d69a12dcba8f6a541a3a2c9cd0bbcfde
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-19 15:57:40 +0530

    refactor: updated the `ResourceType` type

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,7 +1,52 @@
-export type ResourceType = "mysql" | "postgres" | "mongodb" | "rest_api" | "graphql";
+export type ResourceType =
+  // Database
+  | "mysql"
+  | "postgres"
+  | "microsoft_sql"
+  | "mongodb"
+  | "cassandra"
+  | "cosmosdb"
+  | "amazon_redshift"
+  | "amazon_athena"
+  | "bigquery"
+  | "elasticsearch"
+  | "couchdb"
+  | "rethinkdb"
+  | "snowflake"
+  | "denodo"
+  | "redis"
+  | "dynamodb"
+  
+  // APIs
+  | "rest_api"
+  | "graphql"
+  | "firebase"
+  | "stripe"
+  | "twilio"
+  | "github"
+  | "google_sheets"
+  | "salesforce"
+  | "sendgrid"
+  | "amazon_s3"
+  | "google_cloud_storage"
+  | "datadog"
+  | "lambda"
+  | "openapi"
+  | "smtp"
+  | "slack"
+  | "asana"
+  | "jira"
+  | "close"
+  | "bigid"
+  | "basecamp"
+  | "onesignal"
+  | "front"
+  | "google_maps"
+  | "circleci"
+  ;
 
 export interface Resource {
-    type: ResourceType;
-    title: string;
-    imageURL: string;
-};
+  type: ResourceType;
+  title: string;
+  imageURL: string;
+}
