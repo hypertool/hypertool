6ce95551dc6bcb11bb55474aea0899b343cdd9e6
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-19 12:41:12 +0530

    feat: created an empty New Resource screen

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -3,3 +3,4 @@ export { default as Test } from "./test";
 export { default as ViewApps } from "./view-apps";
 export { default as NewApp } from "./new-app";
 export { default as ResourceLibrary } from "./resource-library";
+export { default as NewResource } from "./new-resource";
---
+++ src/screens/new-resource/index.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent, ReactElement } from "react";
+
+const NewResource: FunctionComponent = (): ReactElement => {
+  return <section>New Resource</section>;
+};
+
+export default NewResource;
