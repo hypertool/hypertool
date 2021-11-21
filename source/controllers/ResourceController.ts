5020fdc84f0c8665895977ded4e1959a51be8c88
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-21 22:23:45 +0530

    feat: implement the `create` controller for resources
     * Created the `toExternal` local utility function for converting `ResourceModel` to `ExternalResourceModel`.
       This additional step allows us to filter out sensitive data, transform data to different shapes, and so
       on.
     * Added the `creator` attribute to `createSchema`.

--- source/controllers/ResourceController.ts
+++ source/controllers/ResourceController.ts
@@ -1,10 +1,14 @@
 import joi from "joi";
 
-import { constants } from "../utils";
+import type { Resource } from "../types";
+
+import { constants, BadRequestError } from "../utils";
+import { ResourceModel } from "../models";
 
 const createSchema = joi.object({
   name: joi.string().min(1).max(256).allow(""),
   description: joi.string().min(0).max(512).allow(""),
+  creator: joi.string().regex(constants.identifierPattern).required(),
   type: joi
     .string()
     .valid(...constants.resourceTypes)
@@ -37,3 +41,103 @@ const createSchema = joi.object({
     key: joi.any(),
   }),
 });
+
+interface ExternalMySQLConfiguration {
+  host: string;
+  port: number;
+  databaseName: string;
+  databaseUserName: string;
+  connectUsingSSL: boolean;
+}
+
+interface ExternalPostgresConfiguration {
+  host: string;
+  port: number;
+  databaseName: string;
+  databaseUserName: string;
+  connectUsingSSL: boolean;
+}
+
+interface ExternalMongoDBConfiguration {
+  host: string;
+  port: number;
+  databaseName: string;
+  databaseUserName: string;
+  connectUsingSSL: boolean;
+}
+
+interface ExternalBigQueryConfiguration {
+  [key: string]: any;
+}
+
+interface ExternalResource {
+  name: string;
+  description: string;
+  type: string;
+  configuration:
+    | ExternalMySQLConfiguration
+    | ExternalPostgresConfiguration
+    | ExternalMongoDBConfiguration
+    | ExternalBigQueryConfiguration;
+  status: string;
+}
+
+const toExternal = (resource: Resource): ExternalResource => {
+  const { name, description, type, configuration, status } = resource;
+  let sanitizedConfiguration = null;
+  switch (type) {
+    case "mysql":
+    case "postgres":
+    case "mongodb": {
+      const { host, port, databaseName, databaseUserName, connectUsingSSL } =
+        configuration;
+      sanitizedConfiguration = {
+        host,
+        port,
+        databaseName,
+        databaseUserName,
+        connectUsingSSL,
+      };
+      break;
+    }
+
+    case "bigquery": {
+      sanitizedConfiguration = configuration;
+      break;
+    }
+
+    default: {
+      throw new Error(`Unknown resource type "${type}"`);
+    }
+  }
+
+  return {
+    name,
+    description,
+    type,
+    configuration: sanitizedConfiguration,
+    status,
+  };
+};
+
+const create = async (context, attributes): Promise<ExternalResource> => {
+  const { error, value } = createSchema.validate(attributes, {
+    stripUnknown: true,
+  });
+
+  if (error) {
+    throw new BadRequestError(error.message);
+  }
+
+  // TODO: Check if value.creator is correct.
+  // TODO: Check if value.name is unique across the organization and matches the identifier regex.
+  const newResource = new ResourceModel({
+    ...value,
+    status: "private",
+  });
+  await newResource.save();
+
+  return toExternal(newResource);
+};
+
+export { create };
