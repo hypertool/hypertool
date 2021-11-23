2763b87478900058252da67af6d96439f9a69899
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-23 22:25:32 +0530

    feat: created a GraphQL enum for `ResourceStatus`

--- source/graphql/privateAPI.ts
+++ source/graphql/privateAPI.ts
@@ -1,29 +1,32 @@
 import { ApolloServer, gql } from "apollo-server-express";
 
-import { resourceTypes } from "../utils/constants";
+import { resourceTypes, resourceStatuses } from "../utils/constants";
 
 const typeDefs = gql`
     enum ResourceType {
         ${resourceTypes.join("\n")}
     }
+
+    enum ResourceStatus {
+        ${resourceStatuses.join("\n")}
+    }
 `;
 
-const resolvers = {
-};
+const resolvers = {};
 
 const attachRoutes = async (app: any) => {
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        context: (context) => ({
-            request: context.req
-        })
-    });
-    await server.start();
-    server.applyMiddleware({
-        app,
-        path: "/graphql/v1/private"
-    });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: (context) => ({
+      request: context.req,
+    }),
+  });
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/graphql/v1/private",
+  });
 };
 
 export { attachRoutes };
