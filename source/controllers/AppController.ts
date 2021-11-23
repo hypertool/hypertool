a83932edd43fd49cfaf4cc894a2e4070648a260d
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-23 14:27:55 +0530

    feat: created the `toExternal` utility function

--- source/controllers/AppController.ts
+++ source/controllers/AppController.ts
@@ -1,6 +1,9 @@
 import joi from "joi";
 
+import type { Member } from "../types";
+
 import { constants } from "../utils";
+import { App, ExternalApp } from "../types";
 
 const createSchema = joi.object({
   name: joi.string().min(1).max(128).allow(""),
@@ -9,3 +12,26 @@ const createSchema = joi.object({
   resources: joi.array().items(joi.string().regex(constants.identifierPattern)),
   creator: joi.string().regex(constants.identifierPattern),
 });
+
+const toExternal = (app: App): ExternalApp => {
+  const { name, description, members, resources, creator, status } = app;
+
+  return {
+    name,
+    description,
+    members:
+      members.length > 0
+        ? typeof members[0] === "string"
+          ? members
+          : members.map((member) => member.id)
+        : [],
+    resources:
+      resources.length > 0
+        ? typeof resources[0] === "string"
+          ? resources
+          : resources.map((member) => member.id)
+        : [],
+    creator: typeof creator === "string" ? creator : (creator as Member).id,
+    status,
+  };
+};
