db24c2e7de24f892b88bdc34758dae512167b95b
Joel E. Rego <joelerego@outlook.com>
2021-11-24 00:27:50 +0530

    fix: added `organization` in the `User` interface

--- source/types/index.ts
+++ source/types/index.ts
@@ -112,6 +112,7 @@ export interface User {
   firstName: string;
   lastName: string;
   description: string;
+  organization: string | Organization;
   gender: typeof genders[number];
   countryCode: typeof countryCodes[number];
   pictureURL: string;
