4a41a3c7cbabf932909768123dda1ece9d649c32
Joel E. Rego <joelerego@outlook.com>
2021-11-25 12:31:19 +0530

    feat: created the controllers for `OrganizationModel`

--- source/controllers/OrganizationController.ts
+++ source/controllers/OrganizationController.ts
@@ -1,23 +1,15 @@
 import joi from "joi";
 
-import type { User, UserPage } from "../types";
+import type { Organization } from "../types";
 
 import { constants, BadRequestError, NotFoundError } from "../utils";
-import { UserModel } from "../models";
+import { OrganizationModel } from "../models";
 
 const createSchema = joi.object({
   name: joi.string().min(0).max(256).allow(""),
   description: joi.string().min(0).max(512).allow(""),
-  creator: joi.string().regex(constants.identifierPattern).required(),
-  gender: joi.string().valid(...constants.genders).required(),
-  countryCode: joi.string().valid(...constants.countryCodes).required(),
-  pictureURL: joi.string().allow(""),
-  emailAddress: joi.string().max(256).required(),
-  emailVerified: joi.string().allow(""),
-  birthday: joi.date().allow(null),
-  status: joi.string().valid(...constants.userStatuses),
-  role: joi.string().valid(...constants.userRoles),
-  groups: joi.array().items(joi.string().regex(constants.identifierPattern)),
+  status: joi.string().valid(...constants.organizationStatuses).required(),
+  user: joi.array().items(joi.string().regex(constants.identifierPattern)),
 });
 
 const filterSchema = joi.object({
@@ -31,21 +23,13 @@ const filterSchema = joi.object({
 });
 
 const updateSchema = joi.object({
-    firstName: joi.string().min(1).max(256).required(),
-    lastName: joi.string().max(256).allow("").required(),
-    description: joi.string().min(0).max(512).allow("").required(),
-    gender: joi.string().valid(...constants.genders).required(),
-    countryCode: joi.string().valid(...constants.countryCodes).required(),
-    pictureURL: joi.string().allow(""),
-    emailAddress: joi.string().max(256).required(),
-    emailVerified: joi.string().allow(""),
-    birthday: joi.date().allow(null),
-    status: joi.string().valid(...constants.userStatuses),
-    role: joi.string().valid(...constants.userRoles),
-    groups: joi.array().items(joi.string().regex(constants.identifierPattern)),
+    name: joi.string().min(0).max(256).allow(""),
+    description: joi.string().min(0).max(512).allow(""),
+    status: joi.string().valid(...constants.organizationStatuses).required(),
+    user: joi.array().items(joi.string().regex(constants.identifierPattern)),
 });
 
-const create = async (context, attributes): Promise<User> => {
+const create = async (context, attributes): Promise<Organization> => {
   const { error, value } = createSchema.validate(attributes, {
     stripUnknown: true,
   });
@@ -56,83 +40,22 @@ const create = async (context, attributes): Promise<User> => {
 
   // TODO: Check if value.creator is correct.
   // TODO: Check if value.name is unique across the organization and matches the identifier regex.
-  const newUser = new UserModel({
+  const newOrganization = new OrganizationModel({
     ...value,
     status: "active",
   });
-  await newUser.save();
+  await newOrganization.save();
 
-  return newUser;
-};
-
-const list = async (context, parameters): Promise<UserPage> => {
-  const { error, value } = filterSchema.validate(parameters);
-  if (error) {
-    throw new BadRequestError(error.message);
-  }
-
-  // TODO: Update filters
-  const filters = {
-    status: {
-      $ne: "deleted",
-    },
-  };
-  const { page, limit } = value;
-
-  const users = await (UserModel as any).paginate(filters, {
-    limit,
-    page: page + 1,
-    lean: true,
-    leanWithId: true,
-    pagination: true,
-    sort: {
-      updatedAt: -1,
-    },
-  });
-
-  return {
-    totalRecords: users.totalDocs,
-    totalPages: users.totalPages,
-    previousPage: users.prevPage ? users.prevPage - 1 : -1,
-    nextPage: users.nextPage ? users.nextPage - 1 : -1,
-    hasPreviousPage: users.hasPrevPage,
-    hasNextPage: users.hasNextPage,
-    records: users.docs,
-  };
-};
-
-const getById = async (
-  context,
-  userId: string
-): Promise<User> => {
-  if (!constants.identifierPattern.test(userId)) {
-    throw new BadRequestError("The specified user identifier is invalid.");
-  }
-
-  // TODO: Update filters
-  const filters = {
-    _id: userId,
-    status: { $ne: "deleted" },
-  };
-  const user = await UserModel.findOne(filters as any).exec();
-
-  /* We return a 404 error, if we did not find the user. */
-  if (!user) {
-    throw new NotFoundError(
-      "Cannot find a user with the specified identifier."
-    );
-  }
-
-  return user;
+  return newOrganization;
 };
 
 const update = async (
   context,
-  userId: string,
+  organizationId: string,
   attributes
-): Promise<User> => {
-  if (!constants.identifierPattern.test(userId)) {
-    throw new BadRequestError("The specified user identifier is invalid.");
+): Promise<Organization> => {
+  if (!constants.identifierPattern.test(organizationId)) {
+    throw new BadRequestError("The specified organization identifier is invalid.");
   }
 
   const { error, value } = updateSchema.validate(attributes, {
@@ -143,10 +66,10 @@ const update = async (
   }
 
   // TODO: Update filters
-  const user = await UserModel.findOneAndUpdate(
+  const organization = await OrganizationModel.findOneAndUpdate(
     {
-      _id: userId,
-      status: { $ne: "removed" },
+      _id: organizationId,
+      status: { $ne: "deleted" },
     },
     value,
     {
@@ -155,95 +78,32 @@ const update = async (
     }
   ).exec();
 
-  if (!user) {
+  if (!organization) {
     throw new NotFoundError(
-      "A user with the specified identifier does not exist."
+      "A organization with the specified identifier does not exist."
     );
   }
 
-  return user;
+  return organization;
 };
 
-const activate = async (
-  context,
-  userId: string
-): Promise<User> => {
-  if (!constants.identifierPattern.test(userId)) {
-    throw new BadRequestError("The specified user identifier is invalid.");
-  }
-
-  // TODO: Update filters
-  const user = await UserModel.findOneAndUpdate(
-    {
-      _id: userId,
-      status: { $ne: "removed" },
-    },
-    {
-      status: "activated",
-    },
-    {
-      new: true,
-      lean: true,
-    }
-  );
-
-  if (!user) {
-    throw new NotFoundError(
-      "A user with the specified identifier does not exist."
-    );
-  }
-
-  return user;
-};
-
-const invite = async (
-    context,
-    userId: string
-  ): Promise<User> => {
-    if (!constants.identifierPattern.test(userId)) {
-      throw new BadRequestError("The specified user identifier is invalid.");
-    }
-  
-    // TODO: Update filters
-    const user = await UserModel.findOneAndUpdate(
-      {
-        _id: userId,
-        status: { $ne: "removed" },
-      },
-      {
-        status: "invited",
-      },
-      {
-        new: true,
-        lean: true,
-      }
-    );
-  
-    if (!user) {
-      throw new NotFoundError(
-        "A user with the specified identifier does not exist."
-      );
-    }
-  
-    return user;
-  };
 
 const remove = async (
   context,
-  userId: string
+  organizationId: string
 ): Promise<{ success: boolean }> => {
-  if (!constants.identifierPattern.test(userId)) {
-    throw new BadRequestError("The specified user identifier is invalid.");
+  if (!constants.identifierPattern.test(organizationId)) {
+    throw new BadRequestError("The specified organization identifier is invalid.");
   }
 
   // TODO: Update filters
-  const user = await UserModel.findOneAndUpdate(
+  const organization = await OrganizationModel.findOneAndUpdate(
     {
-      _id: userId,
-      status: { $ne: "removed" },
+      _id: organizationId,
+      status: { $ne: "deleted" },
     },
     {
-      status: "removed",
+      status: "deleted",
     },
     {
       new: true,
@@ -251,17 +111,14 @@ const remove = async (
     }
   );
 
-  if (!user) {
+  if (!organization) {
     throw new NotFoundError(
-      "A user with the specified identifier does not exist."
+      "A organization with the specified identifier does not exist."
     );
   }
 
   return { success: true };
 };
 
-// changeRole (userId, role) -> Make user Owner/Editor/Viewer
-// addToGroup (userId, groupId)
-// removeFromGroup (userId, groupId)
 
-export { create, list, getById, update, activate, invite, remove };
+export { create, update, remove };
