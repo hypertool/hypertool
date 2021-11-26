61a19a0435ec6267fb4632d2b039dfd876d464cc
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-26 22:35:22 +0530

    feat: added form validation to `PostgresForm`
     * Created the `TextField` component that passes Formik related props.
       This helps us reduce repetitive boilerplate code significantly.
     * The new `TextField` component accepts `help` prop that is rendered
       as `helperText` when there is no error.

--- src/screens/new-resource/PostgresForm.tsx
+++ src/screens/new-resource/PostgresForm.tsx
@@ -1,13 +1,50 @@
 import type { FunctionComponent, ReactElement } from "react";
 
 import {
-  TextField,
+  TextField as MuiTextField,
   Typography,
   FormControlLabel,
   Checkbox,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { Formik } from "formik";
+import { Formik, useFormikContext } from "formik";
+import * as yup from "yup";
+
+const TextFieldHelp = styled(Typography)(({ theme }) => ({
+  display: "flex",
+  marginTop: 4,
+  flexDirection: "column",
+  marginLeft: -8,
+  marginBottom: 0,
+  paddingBottom: 0,
+}));
+
+const TextField = (props: {
+  name: string;
+  help: string;
+  [key: string]: any;
+}) => {
+  const { name, help, ...otherProps } = props;
+  const formik = useFormikContext();
+
+  const error = (formik.touched as any)[name] && (formik.errors as any)[name];
+
+  return (
+    <MuiTextField
+      name={name}
+      helperText={
+        <>
+          {error && <TextFieldHelp variant="caption">{error}</TextFieldHelp>}
+          {!error && help}
+        </>
+      }
+      onBlur={formik.handleBlur}
+      onChange={formik.handleChange}
+      error={Boolean(error)}
+      {...otherProps}
+    />
+  );
+};
 
 const ResourceNameTextField = styled(TextField)(({ theme }) => ({
   maxWidth: 400,
@@ -38,15 +75,6 @@ const DatabasePasswordTextField = styled(TextField)(({ theme }) => ({
   marginTop: theme.spacing(2),
 })) as any;
 
-const TextFieldHelp = styled(Typography)(({ theme }) => ({
-  display: "flex",
-  marginTop: 4,
-  flexDirection: "column",
-  marginLeft: -8,
-  marginBottom: 0,
-  paddingBottom: 0,
-}));
-
 const SSLLabel = styled(FormControlLabel)(({ theme }) => ({
   color: theme.palette.getContrastText(theme.palette.background.default),
   marginTop: theme.spacing(1),
@@ -62,82 +90,109 @@ const initialValues = {
   connectUsingSSL: false,
 };
 
+const validationSchema = yup.object({
+  resourceName: yup
+    .string()
+    .max(256, "Resource name should 256 characters or less")
+    .required("Resource name is required"),
+  host: yup.string().required("Host is required"),
+  port: yup
+    .number()
+    .typeError("Port number should be an integer")
+    .integer("Port number should be an integer")
+    .required("Port number is required"),
+  databaseName: yup.string().required("Please specify a valid database name"),
+  databaseUserName: yup
+    .string()
+    .required("Please specify a valid database user name"),
+  databasePassword: yup
+    .string()
+    .required("Please specify a valid database password"),
+  connectUsingSSL: yup.boolean().default(false),
+});
+
 const PostgresForm: FunctionComponent = (): ReactElement => {
   const handleSubmit = () => {};
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <ResourceNameTextField
-        name="resourceName"
-        required={true}
-        id="resourceName"
-        label="Resource Name"
-        size="small"
-        variant="outlined"
-        fullWidth={true}
-        helperText={
-          <TextFieldHelp variant="caption">
-            The resource name will help you identify the resource across
-            Hypertool, including HTX and JavaScript code.
-          </TextFieldHelp>
-        }
-      />
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      <>
+        <ResourceNameTextField
+          name="resourceName"
+          required={true}
+          id="resourceName"
+          label="Resource Name"
+          size="small"
+          variant="outlined"
+          fullWidth={true}
+          help={
+            <TextFieldHelp variant="caption">
+              The resource name will help you identify the resource across
+              Hypertool, including HTX and JavaScript code.
+            </TextFieldHelp>
+          }
+        />
 
-      <HostTextField
-        name="host"
-        required={true}
-        id="host"
-        label="Host"
-        size="small"
-        variant="outlined"
-        fullWidth={true}
-      />
+        <HostTextField
+          name="host"
+          required={true}
+          id="host"
+          label="Host"
+          size="small"
+          variant="outlined"
+          fullWidth={true}
+        />
 
-      <PortTextField
-        name="port"
-        required={true}
-        id="port"
-        label="Port"
-        size="small"
-        variant="outlined"
-        fullWidth={true}
-      />
+        <PortTextField
+          name="port"
+          required={true}
+          id="port"
+          label="Port"
+          size="small"
+          variant="outlined"
+          fullWidth={true}
+        />
 
-      <DatabaseNameTextField
-        name="databaseName"
-        required={true}
-        id="databaseName"
-        label="Database Name"
-        size="small"
-        variant="outlined"
-        fullWidth={true}
-      />
+        <DatabaseNameTextField
+          name="databaseName"
+          required={true}
+          id="databaseName"
+          label="Database Name"
+          size="small"
+          variant="outlined"
+          fullWidth={true}
+        />
 
-      <DatabaseUserNameTextField
-        name="databaseUserName"
-        required={true}
-        id="databaseUserName"
-        label="User Name"
-        size="small"
-        variant="outlined"
-        fullWidth={true}
-      />
+        <DatabaseUserNameTextField
+          name="databaseUserName"
+          required={true}
+          id="databaseUserName"
+          label="User Name"
+          size="small"
+          variant="outlined"
+          fullWidth={true}
+        />
 
-      <DatabasePasswordTextField
-        name="databasePassword"
-        required={true}
-        id="databasePassword"
-        label="Password"
-        size="small"
-        variant="outlined"
-        type="password"
-        fullWidth={true}
-      />
+        <DatabasePasswordTextField
+          name="databasePassword"
+          required={true}
+          id="databasePassword"
+          label="Password"
+          size="small"
+          variant="outlined"
+          type="password"
+          fullWidth={true}
+        />
 
-      <SSLLabel
-        control={<Checkbox name="connectUsingSSL" defaultChecked={false} />}
-        label="Connect using SSL"
-      />
+        <SSLLabel
+          control={<Checkbox name="connectUsingSSL" defaultChecked={false} />}
+          label="Connect using SSL"
+        />
+      </>
     </Formik>
   );
 };
