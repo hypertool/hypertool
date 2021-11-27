879e52a0482a7bba4cf87d986be211e63943e79d
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-27 13:12:05 +0530

    feat: created `Checkbox` component, integerated with Formik

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@ export { default as MiniDrawer } from "./MiniDrawer";
 export { default as PrimaryAppBar } from "./PrimaryAppBar";
 export { default as Wrap } from "./Wrap";
 export { default as TextField } from "./TextField";
+export { default as Checkbox } from "./Checkbox";
