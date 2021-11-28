35bb058958110aee1835bbe1e24c5393d94bdd5a
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-28 12:20:39 +0530

    feat: created `MongoDBConfiguration` interface

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -20,6 +20,15 @@ export interface PostgresConfiguration {
   connectUsingSSL: boolean;
 }
 
+export interface MongoDBConfiguration {
+  host: string;
+  port: number;
+  databaseName: string;
+  databaseUserName: string;
+  databasePassword: string;
+  connectUsingSSL: boolean;
+}
+
 export interface Resource {
   type: ResourceType;
   title: string;
