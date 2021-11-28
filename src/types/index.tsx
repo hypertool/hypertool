6eeeaa726310a9ca5b1d4704bb832e7ca8452774
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-28 12:09:01 +0530

    refactor: moved `ResourceType` enumerations to `resourceTypes` constant
    The constant is then used to create `ResourceType` via `tuple`.

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,51 +1,6 @@
-export const tuple = <T extends string[]>(...values: T) => values;
+import { resourceTypes } from "../utils/constants";
 
-export type ResourceType =
-  // Database
-  | "mysql"
-  | "postgres"
-  | "microsoft_sql"
-  | "mongodb"
-  | "cassandra"
-  | "cosmosdb"
-  | "amazon_redshift"
-  | "amazon_athena"
-  | "bigquery"
-  | "elasticsearch"
-  | "couchdb"
-  | "rethinkdb"
-  | "snowflake"
-  | "denodo"
-  | "redis"
-  | "dynamodb"
-  
-  // APIs
-  | "rest_api"
-  | "graphql"
-  | "firebase"
-  | "stripe"
-  | "twilio"
-  | "github"
-  | "google_sheets"
-  | "salesforce"
-  | "sendgrid"
-  | "amazon_s3"
-  | "google_cloud_storage"
-  | "datadog"
-  | "lambda"
-  | "openapi"
-  | "smtp"
-  | "slack"
-  | "asana"
-  | "jira"
-  | "close"
-  | "bigid"
-  | "basecamp"
-  | "onesignal"
-  | "front"
-  | "google_maps"
-  | "circleci"
-  ;
+export type ResourceType = typeof resourceTypes[number];
 
 export interface Resource {
   type: ResourceType;
