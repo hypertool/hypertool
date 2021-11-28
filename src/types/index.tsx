e4cb988ee81956c231224f2807a70ce9a0d88400
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-28 12:14:34 +0530

    feat: created `MySQLConfiguration` interface

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -2,6 +2,15 @@ import { resourceTypes } from "../utils/constants";
 
 export type ResourceType = typeof resourceTypes[number];
 
+export interface MySQLConfiguration {
+  host: string;
+  port: number;
+  databaseName: string;
+  databaseUserName: string;
+  databasePassword: string;
+  connectUsingSSL: boolean;
+}
+
 export interface Resource {
   type: ResourceType;
   title: string;
