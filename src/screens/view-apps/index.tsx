489cd6e155e5f44a5ce4e4a12ea83259b7ceb4fe
Samuel Rowe <samuelrowe1999@gmail.com>
2021-11-30 12:25:16 +0530

    feat: added workspace toolbar to View Apps screen

--- src/screens/view-apps/index.tsx
+++ src/screens/view-apps/index.tsx
@@ -2,12 +2,19 @@ import type { FunctionComponent, ReactElement } from "react";
 
 import { useState, useCallback } from "react";
 import {
-  Container as MuiContainer,
+  Container,
   Hidden,
   FormControl as MuiFormControl,
   InputLabel,
   Select,
   MenuItem,
+  AppBar,
+  Toolbar,
+  TextField,
+  Icon,
+  InputAdornment,
+  Button,
+  Typography,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
@@ -16,19 +23,45 @@ import AppFilter from "./AppFilter";
 
 const Root = styled("section")(({ theme }) => ({
   width: "100%",
-  padding: theme.spacing(2),
+}));
+
+const Title = styled(Typography)(({ theme }) => ({}));
+
+const WorkspaceToolbar = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+}));
+
+const ActionContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "flex-end",
+}));
+
+const Search = styled(TextField)(({ theme }) => ({
+  width: 264,
+  marginRight: theme.spacing(2),
+})) as any;
+
+const ActionIcon = styled(Icon)(({ theme }) => ({
+  marginRight: theme.spacing(1),
 }));
 
 const FormControl = styled(MuiFormControl)(({ theme }) => ({
-  marginBottom: theme.spacing(2)
+  marginBottom: theme.spacing(2),
 }));
 
-const Container = styled(MuiContainer)(({ theme }) => ({
+const Content = styled(Container)(({ theme }) => ({
   width: "100%",
   display: "flex",
-  flexDirection: "column",
   alignItems: "flex-start",
   justifyContent: "center",
+  marginTop: theme.spacing(2),
+
+  flexDirection: "column",
 
   [theme.breakpoints.up("lg")]: {
     flexDirection: "row",
@@ -86,6 +119,8 @@ const filters = [
 const ViewApps: FunctionComponent<Props> = (): ReactElement => {
   const [filter, setFilter] = useState<string>(filters[0].url);
 
+  const handleCreateNew = useCallback(() => {}, []);
+
   const handleFilterChange = useCallback((event) => {
     setFilter(event.target.value);
   }, []);
@@ -109,7 +144,30 @@ const ViewApps: FunctionComponent<Props> = (): ReactElement => {
 
   return (
     <Root>
-      <Container>
+      <AppBar position="static" elevation={1}>
+        <WorkspaceToolbar>
+          <Title>Resource Library</Title>
+          <ActionContainer>
+            <Search
+              label=""
+              placeholder="Search"
+              size="small"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icon fontSize="small">search</Icon>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button size="small" onClick={handleCreateNew}>
+              <ActionIcon fontSize="small">add_circle</ActionIcon>
+              Create New
+            </Button>
+          </ActionContainer>
+        </WorkspaceToolbar>
+      </AppBar>
+      <Content>
         <Hidden lgDown={true}>
           <AppFilter />
         </Hidden>
@@ -119,7 +177,7 @@ const ViewApps: FunctionComponent<Props> = (): ReactElement => {
             <AppCard id={app.id} title={app.title} />
           ))}
         </Apps>
-      </Container>
+      </Content>
     </Root>
   );
 };
