fa973e28e0334fc05fe25c0ac904920709789830
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-06 00:36:22 +0530

    feat: created a stub for the start command

--- packages/cli/source/index.ts
+++ packages/cli/source/index.ts
@@ -9,6 +9,8 @@ const create = async (): Promise<void> => {};
 
 const eject = async (): Promise<void> => {};
 
+const start = async (): Promise<void> => {};
+
 const configureCommands = (): Command => {
     const program = new Command();
     program.version(packageData.version);
@@ -28,6 +30,14 @@ const configureCommands = (): Command => {
         .description("creates a new app")
         .action(create);
     program.addCommand(createCommand);
+
+    const startCommand = new Command();
+    startCommand
+        .name("start")
+        .alias("s")
+        .description("starts the development server")
+        .action(start);
+    program.addCommand(startCommand);
 
     const ejectCommand = new Command();
     ejectCommand
