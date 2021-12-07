bfe2e42c3e15d657e6040c95522eed0f1be0177d
Joel <joelerego@gmail.com>
2021-12-07 09:58:18 +0530

    refactor: ran `prettier` on all files in the `ui` package

--- packages/ui/src/Header.stories.tsx
+++ packages/ui/src/Header.stories.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Header } from './Header';
+import { Header } from "./Header";
 
 export default {
-  title: 'Example/Header',
-  component: Header,
+    title: "Example/Header",
+    component: Header,
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
-  user: {},
+    user: {},
 };
 
 export const LoggedOut = Template.bind({});
