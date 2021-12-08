0d85a61c7de397fa79c7488dfc370058e4960eca
joel <joelerego@gmail.com>
2021-12-08 11:26:07 +0530

    refactor: renamed master template `args` to `props`

--- packages/ui/src/components/Button/Button.stories.tsx
+++ packages/ui/src/components/Button/Button.stories.tsx
@@ -11,10 +11,9 @@ export default {
     },
 } as Meta;
 
-// Create a master template for mapping args to render the Button component
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+// A master template for mapping props to render the Button component
+const Template: Story<ButtonProps> = (props) => <Button {...props} />;
 
-// Reuse that template for creating different stories
 export const Primary = Template.bind({});
 Primary.args = { label: "Primary", size: "large" };
 
