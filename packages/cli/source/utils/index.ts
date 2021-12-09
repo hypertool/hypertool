fee6c0e3efbf9f4a8ff41cf99ff84cd94f668b9c
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-09 10:36:26 +0530

    feat: created a utility function `isRoot`

--- packages/cli/source/utils/index.ts
+++ packages/cli/source/utils/index.ts
@@ -111,3 +111,5 @@ export const isPortAvailable = (port: number): Promise<boolean> =>
 
         socket.connect(port, "0.0.0.0");
     });
+
+export const isRoot = () => process.getuid && process.getuid() === 0;
