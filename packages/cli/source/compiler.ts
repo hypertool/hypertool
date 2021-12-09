9479af817c5807136eb7884608c849486c83650e
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-09 13:39:59 +0530

    feat: updated `createCompiler` to handle compilation error on startup

--- packages/cli/source/compiler.ts
+++ packages/cli/source/compiler.ts
@@ -2,6 +2,8 @@ import type { Configuration, Compiler } from "webpack";
 
 import webpack from "webpack";
 
+import { logger } from "./utils";
+
 export const prepare = (): Configuration => {
     return {
         mode: "development",
@@ -11,6 +13,12 @@ export const prepare = (): Configuration => {
 
 export const createCompiler = (): Compiler => {
     const configuration = prepare();
-    const compiler = webpack(configuration);
-    return compiler;
+    try {
+        const compiler = webpack(configuration);
+        return compiler;
+    } catch (error: any) {
+        logger.error("Failed to compile");
+        logger.error(error.message);
+        process.exit(1);
+    }
 };
