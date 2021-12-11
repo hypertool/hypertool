e910132c6b6f31acf7c120d58dfc8e8d1359fb06
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-11 12:54:59 +0530

    feat: created utility for finding public URL or path at which the app is served

--- packages/cli/source/config/paths.ts
+++ packages/cli/source/config/paths.ts
@@ -1,5 +1,63 @@
 import fs from "fs";
 import path from "path";
+import { URL } from "url";
+
+/**
+ * Returns a URL or a path with slash at the end.
+ * In production can be URL, abolute path, relative path.
+ * In development always will be an absolute path.
+ * In development can use `path` module functions for operations
+ *
+ * @param {boolean} development
+ * @param {(string|undefined)} homepage a valid url or pathname
+ * @param {(string|undefined)} envPublicUrl a valid url or pathname
+ * @returns {string}
+ */
+const getPublicUrlOrPath = (
+    development: boolean,
+    homepage?: string,
+    envPublicUrl?: string,
+) => {
+    const stubDomain = "https://hypertool.io";
+
+    if (envPublicUrl) {
+        // ensure last slash exists
+        envPublicUrl = envPublicUrl.endsWith("/")
+            ? envPublicUrl
+            : envPublicUrl + "/";
+
+        // validate if `envPublicUrl` is a URL or path like
+        // `stubDomain` is ignored if `envPublicUrl` contains a domain
+        const validPublicUrl = new URL(envPublicUrl, stubDomain);
+
+        return development
+            ? envPublicUrl.startsWith(".")
+                ? "/"
+                : validPublicUrl.pathname
+            : // Some apps do not use client-side routing with pushState.
+              // For these, "homepage" can be set to "." to enable relative asset paths.
+              envPublicUrl;
+    }
+
+    if (homepage) {
+        // strip last slash if exists
+        homepage = homepage.endsWith("/") ? homepage : homepage + "/";
+
+        // validate if `homepage` is a URL or path like and use just pathname
+        const validHomepagePathname = new URL(homepage, stubDomain).pathname;
+        return development
+            ? homepage.startsWith(".")
+                ? "/"
+                : validHomepagePathname
+            : // Some apps do not use client-side routing with pushState.
+            // For these, "homepage" can be set to "." to enable relative asset paths.
+            homepage.startsWith(".")
+            ? homepage
+            : validHomepagePathname;
+    }
+
+    return "/";
+};
 
 export const CWD = process.cwd();
 
@@ -28,5 +86,20 @@ export const PACKAGE_DESCRIPTOR = path.join(APP_DIRECTORY, "package.json");
 export const MANIFEST_DIRECTORY = path.join(APP_DIRECTORY, "manifest");
 
 export const DOT_ENV = path.join(APP_DIRECTORY, ".env");
+
+/* We use `PUBLIC_URL` environment variable or "homepage" field to infer
+ * "public path" at which the app is served.
+ * Webpack needs to know it to put the right `<script>` hrefs into HTML even in
+ * single-page apps that may serve `index.html` for nested URLs like `/todos/42`.
+ * We can't use a relative path in HTML because we don't want to load something
+ * like /todos/42/static/js/bundle.7289d.js.
+ * We have to know the root.
+ */
+export const PUBLIC_URL_OR_PATH = getPublicUrlOrPath(
+    process.env.NODE_ENV === "development",
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require(PACKAGE_DESCRIPTOR).homepage,
+    process.env.PUBLIC_URL,
+);
 
 export const extensions = ["js", "ts", "htx", "json"];
