7ee1641f888d4d55c9c0224cc764e2ae8ed2f567
Joel E. Rego <joelerego@gmail.com>
2021-12-11 05:57:24 +0530

    feat: create the `manifest` compiler to recursively read `YAML` files

--- packages/cli/source/manifest/compiler.ts
+++ packages/cli/source/manifest/compiler.ts
@@ -1,11 +1,16 @@
-import fs from "fs";
 import yaml from "js-yaml";
+import { glob } from "glob";
+import fs from "fs";
 
 import * as paths from "../config/paths";
 
 const compiler = () => {
-    console.log(paths.MANIFEST_DIRECTORY);
-    return null;
+    glob(paths.MANIFEST_DIRECTORY + "**/*.{yml, yaml}", function (err, files) {
+        files.forEach((file) => {
+            const loaded = yaml.load(fs.readFileSync(file, "utf8"));
+            console.log(loaded);
+        });
+    });
 };
 
 export default compiler;
