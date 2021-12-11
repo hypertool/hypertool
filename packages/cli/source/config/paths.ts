c92811c44ef5c030588071e33af6bbc1a0e576a5
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-11 12:55:31 +0530

    docs: added Create React App license header

--- packages/cli/source/config/paths.ts
+++ packages/cli/source/config/paths.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * Copyright (c) 2021-present, Hypertool <hello@hypertool.io>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import fs from "fs";
 import path from "path";
 import { URL } from "url";
