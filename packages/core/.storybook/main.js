f696518ac76fba8450b71cbaf622c410c9ca296c
Joel E. Rego <joelerego@gmail.com>
2021-12-18 00:57:25 +0530

    build: changed `sb` development path to `stories`

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -1,8 +1,7 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+    "../source/**/*.stories.ts"
+ ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
