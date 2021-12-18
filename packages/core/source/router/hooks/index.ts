622dc3d3ba9b6f888c09a0b08ef7f5b6f91f8fe9
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-18 16:03:28 +0530

    feat: created index for hooks module

---
+++ packages/core/source/router/hooks/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2021 - present, Hypertool <hello@hypertool.io>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export { default as useHref } from "./useHref";
+export { default as useInRouterContext } from "./useInRouterContext";
+export { default as useLocation } from "./useLocation";
+export { default as useMatch } from "./useMatch";
+export { default as useNavigate } from "./useNavigate";
+export { default as useNavigationType } from "./useNavigationType";
+export { default as useOutlet } from "./useOutlet";
+export { default as useOutletContext } from "./useOutletContext";
+export { default as useParams } from "./useParams";
+export { default as useResolvedPath } from "./useResolvedPath";
+export { default as useRoutes } from "./useRoutes";
