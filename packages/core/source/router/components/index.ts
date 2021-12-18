1c65cbb22934957cc1292fdfa95abbbe4f3895ea
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-18 16:11:16 +0530

    feat: created index for components module

---
+++ packages/core/source/router/components/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2021 - present, Hypertool <hello@hypertool.io>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export { default as MemoryRouter } from "./MemoryRouter";
+export { default as Navigate } from "./Navigate";
+export { default as NavigationContext } from "./NavigationContext";
+export { default as RouteContext } from "./RouteContext";
+export { default as LocationContext } from "./LocationContext";
+export { default as Outlet } from "./Outlet";
+export { default as Route } from "./Route";
+export { default as Router } from "./Router";
+export { default as Routes } from "./Routes";
+
+export * from "./NavigationContext";
+export * from "./RouteContext";
+export * from "./LocationContext";
+export * from "./Router";
+export * from "./Routes";
