218dd3088bd986228557d032493dfc5d47615604
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-18 16:11:29 +0530

    feat: created index for router module

---
+++ packages/core/source/router/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2015 - 2019, React Training
+ * Copyright (c) 2020 - 2021, Remix Software
+ * Copyright (c) 2021 - present, Hypertool <hello@hypertool.io>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type { Location, Path, To } from "history";
+
+import { Action as NavigationType } from "history";
+
+export type { Location, Path, To, NavigationType };
+
+export * from "./utils";
+export * from "./hooks";
+export * from "./components";
