05e17e6cc6069eb1f79daeaa3c29fae80ab980fc
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-18 15:56:21 +0530

    feat: created `useLocation` hook

---
+++ packages/core/source/router/hooks/useLocation.ts
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2015 - 2019, React Training
+ * Copyright (c) 2020 - 2021, Remix Software
+ * Copyright (c) 2021 - present, Hypertool <hello@hypertool.io>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { useContext } from "react";
+
+import { invariant } from "../utils";
+import { LocationContext } from "../components";
+import useInRouterContext from "./useInRouterContext";
+
+/**
+ * Returns the current location object, which represents the current URL in web
+ * browsers.
+ *
+ * Note: If you're using this it may mean you're doing some of your own
+ * "routing" in your app, and we'd like to know what your use case is. We may
+ * be able to provide something higher-level to better suit your needs.
+ *
+ * @see https://reactrouter.com/docs/en/v6/api#uselocation
+ */
+function useLocation(): Location {
+    invariant(
+        useInRouterContext(),
+        // TODO: This error is probably because they somehow have 2 versions of the
+        // router loaded. We can help them understand how to avoid that.
+        `useLocation() may be used only in the context of a <Router> component.`,
+    );
+
+    return useContext(LocationContext).location;
+}
+
+export default useLocation;
