4a31d2ce97ab44e92a51489eeaea34de4f915858
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-21 03:14:30 +0530

    feat: created `MainFooter` component for visitor layout

---
+++ components/layouts/visitor/MainFooter.tsx
@@ -0,0 +1,177 @@
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Link from "next/link";
+import Image from "next/image";
+
+import LogoImage from "../../../public/logo.png";
+
+const Container = styled("footer")(({ theme }) => ({
+  width: "100%",
+  background: "linear-gradient(45deg, rgba(0,0,0,1) 0%, rgba(60,60,60,1) 100%)",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  paddingLeft: theme.spacing(15),
+  paddingRight: theme.spacing(15),
+  paddingTop: theme.spacing(5),
+
+  [theme.breakpoints.down("sm")]: {
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+  },
+}));
+
+const Top = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  width: "80%",
+  justifyContent: "center",
+  alignItems: "center",
+
+  [theme.breakpoints.up("lg")]: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "flex-start",
+  },
+}));
+
+const Logo = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+
+  [theme.breakpoints.up("lg")]: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+}));
+
+const Right = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  flexWrap: "wrap",
+  flexDirection: "row",
+
+  marginTop: theme.spacing(3),
+
+  [theme.breakpoints.up("lg")]: {
+    marginTop: 0,
+  },
+}));
+
+const Column = styled("div")(({ theme }) => ({
+  width: 200,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+
+  [theme.breakpoints.up("lg")]: {
+    alignItems: "flex-start",
+  },
+}));
+
+const GroupTitle = styled("div")(({ theme }) => ({
+  fontSize: 14,
+  fontWeight: "bold",
+  color: "#707070",
+
+  [theme.breakpoints.down("sm")]: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const GroupItem = styled("a")(({ theme }) => ({
+  fontSize: 14,
+  marginTop: theme.spacing(1),
+  textDecoration: "none",
+  color: "white",
+}));
+
+const Copyright = styled("div")(({ theme }) => ({
+  fontSize: 14,
+  marginTop: theme.spacing(3),
+  color: "white",
+  padding: theme.spacing(3),
+  [theme.breakpoints.down("sm")]: {
+    marginTop: theme.spacing(4),
+  },
+}));
+
+const CompanyTitle = styled(Typography)({
+  fontSize: 18,
+});
+
+const groups = [
+  {
+    title: "Company",
+    children: [
+      {
+        title: "Pricing",
+        url: "/pricing",
+      },
+      {
+        title: "Articles",
+        url: "/articles",
+      },
+    ],
+  },
+  {
+    title: "Connect",
+    children: [
+      {
+        title: "hello@hypertool.io",
+        url: "mailto:hello@hypertool.io",
+      },
+      {
+        title: "+91 8310843835",
+        url: "tel:+918310843835",
+      },
+      {
+        title: "Instagram",
+        url: "https://instagram.com/hypertool",
+      },
+      {
+        title: "Twitter",
+        url: "https://twitter.com/hypertool",
+      },
+      {
+        title: "LinkedIn",
+        url: "https://linkedin.com/company/hypertool",
+      },
+      {
+        title: "GitHub",
+        url: "https://github.com/hypertool",
+      },
+    ],
+  },
+];
+
+const MainFooter = () => {
+  return (
+    <Container>
+      <Top>
+        <Logo>
+          <Image src={LogoImage} width={56} height={56} alt="Logo" />
+          <CompanyTitle>Hypertool</CompanyTitle>
+        </Logo>
+        <Right>
+          {groups.map((group) => (
+            <Column key={group.title}>
+              <GroupTitle>{group.title}</GroupTitle>
+              {group.children.map((item) => (
+                <Link href={item.url} key={item.title} passHref={true}>
+                  <GroupItem>{item.title}</GroupItem>
+                </Link>
+              ))}
+            </Column>
+          ))}
+        </Right>
+      </Top>
+      <Copyright>Copyright 2021 Hypertool</Copyright>
+    </Container>
+  );
+};
+
+export default MainFooter;
