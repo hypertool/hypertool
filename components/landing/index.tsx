98bb6cfaa3be03d56837f5a127edb6eeed0f6c9e
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-28 00:42:44 +0530

    feat: created the `Problems` component

--- components/landing/index.tsx
+++ components/landing/index.tsx
@@ -1,2 +1,3 @@
 export { default as Hero } from "./Hero";
-export { default as Features } from './Features';
+export { default as Features } from "./Features";
+export { default as Problems } from "./Problems";
