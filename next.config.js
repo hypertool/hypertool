9e84f5e2b6d3e679cf218bb1c17f6206da8e5f16
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-28 01:29:17 +0530

    build: added base path and asset prefix in NextJS config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  basePath: "/hypertool.github.io",
+  assetPrefix: "/hypertool.github.io/",
   reactStrictMode: true,
   exportPathMap: async () => ({
     "/": { page: "/" },
