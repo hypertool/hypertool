b097ef0abef9b8821270a5a4fdd9336dc2bd8d93
Samuel Rowe <samuelrowe1999@gmail.com>
2021-12-29 23:43:02 +0530

    build: updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: "/hypertool.github.io",
-  assetPrefix: "/hypertool.github.io/",
   reactStrictMode: true,
-  exportPathMap: async () => ({
-    "/": { page: "/" },
-  }),
 };
