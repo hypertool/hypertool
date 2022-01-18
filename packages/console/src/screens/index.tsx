60051013d4d6a554f598d607043fcbce1c365984
Joel E. Rego <joelerego@gmail.com>
2022-01-18 13:26:54 +0530

    refactor: removed all occurences of `htx` from the console

--- packages/console/src/screens/index.tsx
+++ packages/console/src/screens/index.tsx
@@ -1,5 +1,4 @@
 export { default as Login } from "./login";
-export { default as Test } from "./test";
 export { default as ViewApps } from "./view-apps";
 export { default as NewApp } from "./new-app";
 export { default as ResourceLibrary } from "./resource-library";
