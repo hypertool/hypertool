894c9b8be2d6b6c4c68386b04d09c6d776bd2428
Pratik1234agarwal <pratik1234agarwal@gmail.com>
2022-01-18 12:37:01 +0530

    fix: changed `RoleModel` parameters

--- packages/api/source/models/RoleModel.ts
+++ packages/api/source/models/RoleModel.ts
@@ -1,16 +1,23 @@
 import { Schema, model } from "mongoose";
 import type { Role } from "../types";
 
-const RoleSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    privildges: [
-        {
+const RoleSchema = new Schema(
+    {
+        name: {
             type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 512,
         },
-    ],
-});
+        privildges: [
+            {
+                type: String,
+                minlength: 1,
+                maxlength: 512,
+            },
+        ],
+    },
+    { timestamps: true },
+);
 
 export default model<Role>("Role", RoleSchema);
