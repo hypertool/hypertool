a3b469c34150ec07bc289d25e2bd0341016ac1c8
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-01-19 14:51:30 +0530

    fix: removed unecessary default values from `activityLogSchema`

--- packages/api/source/models/ActivityLogModel.ts
+++ packages/api/source/models/ActivityLogModel.ts
@@ -3,19 +3,17 @@ import { Schema, model } from "mongoose";
 import type { ActivityLog } from "../types";
 import { componentOrigins } from "../utils/constants";
 
-const logSchema = new Schema(
+const activityLogSchema = new Schema(
     {
         message: {
             type: String,
             minlength: 1,
             maxlength: 512,
             required: true,
-            default: "No message to display",
         },
         component: {
             type: String,
             enum: componentOrigins,
-            default: "api",
             required: true,
         },
         context: {
@@ -27,4 +25,4 @@ const logSchema = new Schema(
     },
 );
 
-export default model<ActivityLog>("ActivityLog", logSchema);
+export default model<ActivityLog>("ActivityLog", activityLogSchema);
