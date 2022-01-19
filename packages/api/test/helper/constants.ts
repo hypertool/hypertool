36ab062d68fd425961c77e3c2fe21921941ad3ba
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-01-19 14:44:42 +0530

    feat: created `activityLog` constant

---
+++ packages/api/test/helper/constants.ts
@@ -0,0 +1,22 @@
+const activityLogs = [
+    {
+        context: { type: "abc" },
+        component: "team-management",
+        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante risus, dapibus vitae ultrices a, placerat id augue. Ut at tellus nec dui hendrerit scelerisque. Donec lacinia, ex et congue fermentum, lectus enim hendrerit enim, ac sodales nisl est sed risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce.`,
+    },
+    {
+        context: {
+            type: "xyz",
+            field1: [{ x: 1 }, { y: 2 }],
+            field2: { subfiled: "lorem ipsum dolor amet" },
+        },
+        component: "api",
+        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante risus, dapibus vitae ultrices a, placerat id augue. Ut at tellus nec dui hendrerit scelerisque. Donec lacinia, ex et congue fermentum, lectus enim hendrerit enim, ac sodales nisl est sed risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce.`,
+    },
+    {
+        component: "authorization",
+        message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante risus, dapibus vitae ultrices a, placerat id augue. Ut at tellus nec dui hendrerit scelerisque. Donec lacinia, ex et congue fermentum, lectus enim hendrerit enim, ac sodales nisl est sed risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce.`,
+    },
+];
+
+export { activityLogs };
