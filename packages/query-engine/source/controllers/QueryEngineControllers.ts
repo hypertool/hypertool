e0495a1d91016773c8a52c2bbb115274085793d7
Joel E. Rego <joelerego@gmail.com>
2022-01-19 07:57:06 +0530

    build: replaced the `mysql` driver with `mysql2`

--- packages/query-engine/source/controllers/QueryEngineControllers.ts
+++ packages/query-engine/source/controllers/QueryEngineControllers.ts
@@ -1,6 +1,9 @@
+import mysql from "mysql2";
+
 import type { Document } from "mongoose";
 
 import { constants } from "../utils";
+import { QueryTemplateModel, ResourceModel } from "../models";
 
 const { httpStatuses } = constants;
 
@@ -10,7 +13,27 @@ function toExternal(result) {
 
 const attachRoutes = (router: any): void => {
     router.post("/mysql", async (request, response) => {
-        console.log(request.body);
+        const { name, variables, format } = request.body;
+
+        const query = await QueryTemplateModel.findOne({
+            name,
+        }).exec();
+
+        const { mysql: mySQLConfig } = await ResourceModel.findOne({
+            _id: query.resource,
+        }).exec();
+
+        const connection = mysql.createConnection({
+            host: mySQLConfig.host + ":" + mySQLConfig.port,
+            user: mySQLConfig.databaseUserName,
+            password: mySQLConfig.databasePassword,
+            database: mySQLConfig.databaseName,
+        });
+
+        connection.connect();
+        console.log(
+            " ✅ Client MySQL database connection successfully established",
+        );
 
         return response.status(httpStatuses.OK).json({ hello: "world" });
     });
