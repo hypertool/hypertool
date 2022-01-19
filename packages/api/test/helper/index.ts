4c29f391998dc2e7cd61519a375c467abb7d5ff8
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-01-19 14:47:44 +0530

    feat: created utility function `assertThrowsAsync`

---
+++ packages/api/test/helper/index.ts
@@ -0,0 +1,31 @@
+import { activityLogs } from "./constants";
+import { assert } from "chai";
+
+const assertThrowsAsync = async (
+    callback,
+    assertMessage,
+    type = null,
+    errorMessage = null,
+) => {
+    try {
+        await callback();
+    } catch (error) {
+        if (type) {
+            assert.instanceOf(
+                error,
+                type,
+                `Exception should be of type ${type.name}`,
+            );
+        }
+
+        if (errorMessage) {
+            assert.strictEqual(error.message, errorMessage);
+        }
+
+        return;
+    }
+
+    assert.fail(assertMessage || "Expected an exception to be thrown");
+};
+
+export { assertThrowsAsync, activityLogs };
