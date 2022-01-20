a51916fb03cf0456f1c1f1e13ed0099d90aebbe2
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-01-20 14:18:23 +0530

    feat: created `AuthenticationServicesTypes`

--- packages/console/src/types/index.tsx
+++ packages/console/src/types/index.tsx
@@ -5,42 +5,48 @@ export type ResourceType = typeof resourceTypes[number];
 export type ResourceStatus = typeof resourceStatuses[number];
 
 export interface MySQLConfiguration {
-  host: string;
-  port: number;
-  databaseName: string;
-  databaseUserName: string;
-  connectUsingSSL: boolean;
+    host: string;
+    port: number;
+    databaseName: string;
+    databaseUserName: string;
+    connectUsingSSL: boolean;
 }
 
 export interface PostgresConfiguration {
-  host: string;
-  port: number;
-  databaseName: string;
-  databaseUserName: string;
-  connectUsingSSL: boolean;
+    host: string;
+    port: number;
+    databaseName: string;
+    databaseUserName: string;
+    connectUsingSSL: boolean;
 }
 
 export interface MongoDBConfiguration {
-  host: string;
-  port: number;
-  databaseName: string;
-  databaseUserName: string;
-  connectUsingSSL: boolean;
+    host: string;
+    port: number;
+    databaseName: string;
+    databaseUserName: string;
+    connectUsingSSL: boolean;
 }
 
 export interface BigQueryConfiguration {
-  [key: string]: any;
+    [key: string]: any;
 }
 
 export interface Resource {
-  id: string;
-  name: string;
-  description: string;
-  type: ResourceType;
-  configuration:
-    | MySQLConfiguration
-    | PostgresConfiguration
-    | MongoDBConfiguration
-    | BigQueryConfiguration;
-  status: ResourceStatus;
+    id: string;
+    name: string;
+    description: string;
+    type: ResourceType;
+    configuration:
+        | MySQLConfiguration
+        | PostgresConfiguration
+        | MongoDBConfiguration
+        | BigQueryConfiguration;
+    status: ResourceStatus;
+}
+
+export interface AuthenticationServicesType {
+    id: number;
+    name: string;
+    description: string;
 }
