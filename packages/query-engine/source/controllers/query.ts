e5303750211bd523faf4b28054b69c065e86a55d
Joel E. Rego <joelerego@gmail.com>
2022-01-21 12:22:28 +0530

    feat: created the `queryEngine` controller

---
+++ packages/query-engine/source/controllers/query.ts
@@ -0,0 +1,58 @@
+import mysql from "mysql2";
+
+import type {
+    MySQLConfiguration,
+    Resource,
+    Query,
+    QueryRequest,
+} from "@hypertool/common";
+
+import {
+    QueryTemplateModel,
+    ResourceModel,
+    constants,
+} from "@hypertool/common";
+
+const { httpStatuses } = constants;
+
+const attachRoutes = (router: any): void => {
+    router.post("/mysql", async (request, response) => {
+        const queryRequest: QueryRequest = request.body;
+
+        const query: Query = await QueryTemplateModel.findOne({
+            name: queryRequest.name,
+        }).exec();
+
+        const resource: Resource = await ResourceModel.findOne({
+            _id: query.resource,
+        }).exec();
+
+        const mySQLConfig: MySQLConfiguration = resource.mysql;
+
+        const connection = mysql.createConnection({
+            host: mySQLConfig.host,
+            port: mySQLConfig.port,
+            user: mySQLConfig.databaseUserName,
+            password: mySQLConfig.databasePassword,
+            database: mySQLConfig.databaseName,
+            namedPlaceholders:
+                typeof queryRequest.variables === "object" ? true : false,
+        });
+
+        connection.execute(
+            query.content,
+            queryRequest.variables,
+            (error, results, fields) => {
+                if (error) {
+                    response.status(httpStatuses.UNAUTHORIZED).json({ error });
+                } else {
+                    response.status(httpStatuses.OK).json({ results, fields });
+                }
+            },
+        );
+
+        return response;
+    });
+};
+
+export { attachRoutes };
