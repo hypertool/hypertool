156de67d89c7d4528b842cee2dd2567dac6676d3
Samuel Rowe <samuelrowe1999@gmail.com>
2022-01-24 19:47:30 +0530

    fix: imported `React` in `*.tsx` files

--- packages/core/source/screens/Error404.tsx
+++ packages/core/source/screens/Error404.tsx
@@ -1,7 +1,9 @@
 import type { FunctionComponent, ReactElement } from "react";
 
+import React from "react";
+
 const Error404: FunctionComponent = (): ReactElement => {
-  return <div>Error 404</div>;
+    return <div>Error 404</div>;
 };
 
 export default Error404;
