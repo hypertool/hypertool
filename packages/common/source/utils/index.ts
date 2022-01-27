2f150f398ca6bc20c523e3a6e449c862e758397d
Joel E. Rego <joelerego@gmail.com>
2022-01-27 16:14:01 +0530

    feat: exported the default export as `Client`

--- packages/common/source/utils/index.ts
+++ packages/common/source/utils/index.ts
@@ -24,5 +24,5 @@ export const extractIds = (items: ObjectWithID[] | string[]): string[] => {
 
 export * as constants from "./constants";
 export * as google from "./google";
-export * as client from "./client";
+export { default as Client } from "./client";
 export * from "./errors";
