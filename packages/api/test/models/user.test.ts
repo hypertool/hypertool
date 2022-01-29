869c77690fbd68c51bbe6c2b5112f29e9668c4ee
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-01-29 15:10:34 +0530

    test: created unit test for `lastName` of `UserModel`

--- packages/api/test/models/user.test.ts
+++ packages/api/test/models/user.test.ts
@@ -82,4 +82,65 @@ describe("User Model", function () {
             "The firstName attribute is required.",
         );
     });
+
+    it("should be created when lastName length is 1", async () => {
+        const newUser = new UserModel({
+            ...user,
+            lastName: "A",
+        });
+        await newUser.save();
+
+        assert.lengthOf(
+            newUser.lastName,
+            1,
+            "The lastName attribute should be of length 1.",
+        );
+    });
+
+    it("should not be created when lastName is too long", async () => {
+        const newUser = new UserModel({
+            ...user,
+            lastName: new Array(31 + 1).join("a"),
+        });
+        await assertThrowsAsync(
+            async () => newUser.save(),
+            "The name attribute should have a maximum of 30 characters.",
+        );
+    });
+
+    it("should not be created when lastName is undefined", async () => {
+        const newUser = new UserModel({
+            ...user,
+            lastName: undefined,
+        });
+
+        await assertThrowsAsync(
+            async () => newUser.save(),
+            "The lastName attribute is required.",
+        );
+    });
+
+    it("should not be created when lastName is null", async () => {
+        const newUser = new UserModel({
+            ...user,
+            lastName: null,
+        });
+
+        await assertThrowsAsync(
+            async () => newUser.save(),
+            "The lastName attribute is required.",
+        );
+    });
+
+    it("should not be created when lastName is empty", async () => {
+        const newUser = new UserModel({
+            ...user,
+            lastName: "",
+        });
+
+        await assertThrowsAsync(
+            async () => newUser.save(),
+            "The lastName attribute is required.",
+        );
+    });
 });
