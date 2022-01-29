181e282cf186b6bec9b4f497a5e87f02b0165593
Joel E. Rego <joelerego@gmail.com>
2022-01-29 16:35:05 +0530

    feat: created `BadRequestError` middleware for `express`

--- packages/query-engine/source/utils/errors/BadRequestError.ts
+++ packages/query-engine/source/utils/errors/BadRequestError.ts
@@ -1,3 +1,19 @@
-const BadRequestError = (message) => {};
+import { NextFunction, Response, Request } from "express";
+import { constants } from "@hypertool/common";
+
+const { httpStatuses } = constants;
+
+const BadRequestError = (
+    error: any,
+    request: Request,
+    response: Response,
+    next: NextFunction,
+): void => {
+    response.status(httpStatuses.BAD_REQUEST).json({
+        error: "Bad Request",
+        message: error.message,
+    });
+    next(error);
+};
 
 export default BadRequestError;
