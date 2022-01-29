d43b25c97e771e0431ec966d017ddf843d6abb80
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-01-29 19:50:40 +0530

    test: created unit test for `name` of `ResourceModel`

--- packages/api/test/models/resource.test.ts
+++ packages/api/test/models/resource.test.ts
@@ -21,4 +21,53 @@ describe("Resource Model", function () {
             "The resource should be persisted to the database.",
         );
     });
+
+    it("should be created when name is empty", async () => {
+        const newResource = new ResourceModel({
+            ...resource,
+            name: "",
+        });
+        await newResource.save();
+
+        assert.lengthOf(
+            newResource.name,
+            0,
+            "The name attribute should be of length 0.",
+        );
+    });
+
+    it("should not be created when name is too long", async () => {
+        const newResource = new ResourceModel({
+            ...resource,
+            name: new Array(257 + 1).join("a"),
+        });
+        await assertThrowsAsync(
+            async () => newResource.save(),
+            "The name attribute should have a maximum of 256 characters.",
+        );
+    });
+
+    it("should not be created when name is undefined", async () => {
+        const newResource = new ResourceModel({
+            ...resource,
+            name: undefined,
+        });
+
+        await assertThrowsAsync(
+            async () => newResource.save(),
+            "The name attribute is required.",
+        );
+    });
+
+    it("should not be created when name is null", async () => {
+        const newResource = new ResourceModel({
+            ...resource,
+            name: null,
+        });
+
+        await assertThrowsAsync(
+            async () => newResource.save(),
+            "The name attribute is required.",
+        );
+    });
 });
