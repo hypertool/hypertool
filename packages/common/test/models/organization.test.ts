9230d267769e3a83bf2bc27e080c9412e0d60b99
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-01 23:21:39 +0530

    test: created unit test for `name` of `OrganizationModel`

--- packages/common/test/models/organization.test.ts
+++ packages/common/test/models/organization.test.ts
@@ -22,4 +22,65 @@ describe("Organization model", function () {
             "The log should be persisted to the database.",
         );
     });
+
+    it("should not be created when name is undefined", async () => {
+        const newOrganization = new OrganizationModel({
+            ...organization,
+            name: undefined,
+        });
+
+        await assertThrowsAsync(
+            async () => newOrganization.save(),
+            "The name attribute is required.",
+        );
+    });
+
+    it("should not be created when name is null", async () => {
+        const newOrganization = new OrganizationModel({
+            ...organization,
+            name: null,
+        });
+
+        await assertThrowsAsync(
+            async () => newOrganization.save(),
+            "The name attribute is required.",
+        );
+    });
+
+    it("should not be created when name is empty", async () => {
+        const newOrganization = new OrganizationModel({
+            ...organization,
+            name: "",
+        });
+
+        await assertThrowsAsync(
+            async () => newOrganization.save(),
+            "The name attribute is required.",
+        );
+    });
+
+    it("should be created when name length is 1", async () => {
+        const newOrganization = new OrganizationModel({
+            ...organization,
+            name: "A",
+        });
+        await newOrganization.save();
+
+        assert.lengthOf(
+            newOrganization.name,
+            1,
+            "The name attribute should be of length 1.",
+        );
+    });
+
+    it("should not be created when name is too long", async () => {
+        const newOrganization = new OrganizationModel({
+            ...organization,
+            name: new Array(257 + 1).join("A"),
+        });
+        await assertThrowsAsync(
+            async () => newOrganization.save(),
+            "The name attribute should have a maximum of 256 characters.",
+        );
+    });
 });
