d40a647483029e3b8506bfef1c6e663da688188f
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-01 23:15:46 +0530

    feat: modified `OrganizationModel`

--- packages/common/source/models/OrganizationModel.ts
+++ packages/common/source/models/OrganizationModel.ts
@@ -5,42 +5,58 @@ import type { Organization } from "../types";
 import { organizationStatuses } from "../utils/constants";
 
 const organizationSchema = new Schema(
-  {
-    name: {
-      type: String,
-      minlength: 1,
-      maxlength: 256,
-      required: true,
-      trim: true,
-    },
-    description: {
-      type: String,
-      minlength: 0,
-      maxlength: 512,
-      default: "",
-    },
-    users: {
-      type: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "User",
+    {
+        name: {
+            type: String,
+            minlength: 1,
+            maxlength: 256,
+            required: true,
+            trim: true,
         },
-      ],
-      required: true,
+        title: {
+            type: String,
+            minlength: 1,
+            maxlength: 256,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            minlength: 0,
+            maxlength: 512,
+            default: "",
+        },
+        members: {
+            type: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: "User",
+                },
+            ],
+            required: true,
+        },
+        apps: {
+            type: [
+                {
+                    type: Schema.Types.ObjectId,
+                    ref: "App",
+                },
+            ],
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: organizationStatuses,
+            default: "active",
+        },
     },
-    status: {
-      type: String,
-      enum: organizationStatuses,
-      default: "active",
+    {
+        timestamps: true,
     },
-  },
-  {
-    timestamps: true,
-  }
 );
 
 organizationSchema.index({
-  name: "text",
+    name: "text",
 });
 organizationSchema.plugin(paginate);
 
