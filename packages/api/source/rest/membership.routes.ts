c29f87feac7c13f2007373c73e0ec1e8c61f6b1e
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-06 20:06:16 +0530

    feat: added membership routes to verify membership

---
+++ packages/api/source/rest/membership.routes.ts
@@ -0,0 +1,27 @@
+import type { Router, Request, Response, NextFunction } from "express";
+import { constants } from "@hypertool/common";
+
+import { memberships } from "../controllers";
+
+const { httpStatuses } = constants;
+
+const attachRoutes = (router: Router): void => {
+    router.get(
+        "/invitation/accept/:jwt",
+        async (request: Request, response: Response, next: NextFunction) => {
+            const { jwt } = request.params;
+            const result = await memberships.verify(null, jwt);
+
+            if (result)
+                return response
+                    .status(httpStatuses.OK)
+                    .json({ message: "Invitation Accepted." });
+            else
+                return response
+                    .status(httpStatuses.BAD_REQUEST)
+                    .json({ message: "Invitation link has expired." });
+        },
+    );
+};
+
+export { attachRoutes };
