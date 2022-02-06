c6fb77853752faed2c68e0e2445a230a36c43cfb
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-06 19:56:05 +0530

    build: added `@sendgrid/mail` as dependency

--- packages/common/source/models/index.ts
+++ packages/common/source/models/index.ts
@@ -5,8 +5,5 @@ export { default as ResourceModel } from "./ResourceModel";
 export { default as UserModel } from "./UserModel";
 export { default as QueryTemplateModel } from "./QueryTemplateModel";
 export { default as ActivityLogModel } from "./ActivityLogModel";
-<<<<<<< HEAD
 export { default as DeploymentModel } from "./DeploymentModel";
-=======
 export { default as MembershipModel } from "./MembershipModel";
->>>>>>> 94a7c49... feat: exported `MembershipModel` from index
