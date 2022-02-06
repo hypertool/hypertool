f7e4c03a18eba93689ee55a286cf2707c12d6b13
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-06 20:00:11 +0530

    feat: added `runAsTransaction` function in utils

--- packages/common/source/utils/index.ts
+++ packages/common/source/utils/index.ts
@@ -42,6 +42,21 @@ export const sendEmail = async (emailAddress, subject, text, html) => {
     }
 };
 
+export const runAsTransaction = async (callback) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const result = await callback();
+        await session.commitTransaction();
+        return result;
+    } catch (error) {
+        await session.abortTransaction();
+        throw error;
+    } finally {
+        session.endSession();
+    }
+};
+
 export * as constants from "./constants";
 export * as google from "./google";
 export * as session from "./session";
