296e1888c3ff4ff95a668efc366c59913db62a99
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-02-10 13:39:41 +0530

    feat: exported `Sign-up` page

--- packages/console/src/screens/index.tsx
+++ packages/console/src/screens/index.tsx
@@ -1,4 +1,8 @@
 export { default as Login } from "./login";
+<<<<<<< HEAD
+=======
+export { default as SignUp } from "./sign-up";
+>>>>>>> a93a0e5 (feat: exported `Sign-up` page)
 export { default as ViewApps } from "./view-apps";
 export { default as NewApp } from "./new-app";
 export { default as ResourceLibrary } from "./resource-library";
@@ -7,4 +11,7 @@ export { default as EditResource } from "./edit-resource";
 export { default as NewOrganization } from "./new-organization";
 export { default as SingleApp } from "./single-app";
 export { default as AuthenticationServices } from "./authentication-services";
+<<<<<<< HEAD
 export { default as AppBuilder } from "./app-builder";
+=======
+>>>>>>> a93a0e5 (feat: exported `Sign-up` page)
