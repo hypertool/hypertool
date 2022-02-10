8b42beedf50f9fd9aff1a6d957e20f4b34df0671
Samuel Rowe <samuelrowe1999@gmail.com>
2022-02-10 15:23:25 +0530

    feat: updated the deployment algorithm in `generateSignedURLs` endpoint

--- packages/api/source/controllers/deployments.ts
+++ packages/api/source/controllers/deployments.ts
@@ -1,30 +1,94 @@
-import joi from "joi";
-import { Types } from "mongoose";
-import { google, BadRequestError } from "@hypertool/common";
+import type { App, ExternalDeployment, Deployment } from "@hypertool/common";
 
-const generateSchema = joi.array().items(joi.string());
+import joi from "joi";
+import {
+    google,
+    AppModel,
+    DeploymentModel,
+    BadRequestError,
+    NotFoundError,
+} from "@hypertool/common";
+import mongoose from "mongoose";
+
+const runAsTransaction = async (callback) => {
+    return await callback();
+};
+
+const generateSchema = joi.object({
+    appId: joi.string(),
+    files: joi.array().items(joi.string()),
+});
+
+interface GenerateSignedURLsAttributes {
+    appId: string;
+    files: string[];
+}
+
+interface GenerateSignedURLsResult {
+    signedURLs: string[];
+    deployment: ExternalDeployment;
+}
+
+const toExternal = (deployment: Deployment): ExternalDeployment => {
+    const { _id, app, createdAt, updatedAt } = deployment;
+    return {
+        id: _id.toString(),
+        app:
+            typeof app === "string"
+                ? app
+                : app instanceof mongoose.Types.ObjectId
+                ? app.toString()
+                : (<App>app)._id.toString(),
+        createdAt,
+        updatedAt,
+    };
+};
 
 export const generateSignedURLs = async (
     context,
-    files: string[],
-): Promise<string[]> => {
-    const { error, value: files0 } = generateSchema.validate(files);
+    attributes: GenerateSignedURLsAttributes,
+): Promise<GenerateSignedURLsResult> => {
+    const { error, value } = generateSchema.validate(attributes);
 
     if (error) {
         throw new BadRequestError(error.message);
     }
 
-    const deploymentId = new Types.ObjectId();
+    const { app, deployment } = await runAsTransaction(async () => {
+        const deploymentId = new mongoose.Types.ObjectId();
+        const app = await AppModel.findByIdAndUpdate(value.appId, {
+            $push: {
+                deployments: deploymentId,
+            },
+        });
+        if (!app) {
+            throw new NotFoundError(
+                "Cannot find an app with the specified identifier.",
+            );
+        }
 
-    // TODO: Prefix `fileName` with organization ID
+        const deployment = new DeploymentModel({
+            _id: deploymentId,
+            app: value.appId,
+        });
+        await deployment.save();
+
+        return { app, deployment };
+    });
+
     const promises = [];
-    for (const file of files0) {
+    for (const file of value.files) {
         const promise = google.generateUploadSignedURL(
-            "hypertool-client-builds-asia",
-            `organizationId/${deploymentId.toString()}/${file}`,
+            process.env.APP_BUNDLES_BUCKET_NAME,
+            `${
+                app.organization
+            }/${app._id.toString()}/${deployment._id.toString()}/${file}`,
         );
         promises.push(promise);
     }
 
-    return await Promise.all(promises);
+    return {
+        signedURLs: await Promise.all(promises),
+        deployment: toExternal(deployment),
+    };
 };
