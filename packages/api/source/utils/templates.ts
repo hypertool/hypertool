f41f1c5ee940e48d6bd2a0f64408208ac5f118fa
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-11 21:24:11 +0530

    refactor: promisify `fs`

--- packages/api/source/utils/templates.ts
+++ packages/api/source/utils/templates.ts
@@ -1,8 +1,11 @@
 import Mustache from "mustache";
+import fs from "fs";
 
-const fs = require("fs").promises;
+const fsPromisify = fs.promises;
 
 export const invitationTemplate = async (data) => {
-    const str = await fs.readFile("../templates/invitation-template.html");
+    const str = await fsPromisify.readFile(
+        "../templates/invitation-template.html",
+    );
     return Mustache.render(str, data);
 };
