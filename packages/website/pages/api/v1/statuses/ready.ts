b5251ebf2d8e66756f302c89d7779554bc8fbc8f
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-02-12 12:05:54 +0530

    feat: fixed type error in `website/api/v1/statuses/ready.ts`

--- packages/website/pages/api/v1/statuses/ready.ts
+++ packages/website/pages/api/v1/statuses/ready.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default (request: NextApiRequest, response: NextApiResponse) => {
     response.status(200).json({
-        message: "he @hypertool/api service is ready to accept requests.",
+        message: "The @hypertool/website service is ready to accept requests.",
     });
 };
