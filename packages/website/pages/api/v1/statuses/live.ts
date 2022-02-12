b626e325ecb57858bab2df8924846acdd6ad20a5
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-02-12 11:54:31 +0530

    feat: added liveness probe REST APIs to `website`

---
+++ packages/website/pages/api/v1/statuses/live.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    response
+        .status(200)
+        .json({ message: "The @hypertool/website service is active." });
+};
