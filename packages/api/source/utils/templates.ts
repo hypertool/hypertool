79ecf97a90081245b2fd7c7239529e53a4b31408
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-12 11:09:33 +0530

    feat: changed invitation template

--- packages/api/source/utils/templates.ts
+++ packages/api/source/utils/templates.ts
@@ -2,7 +2,7 @@ import Mustache from "mustache";
 import fs from "fs";
 
 const fsPromisify = fs.promises;
-const invitationTemplatePath = `${process.cwd()}/source/templates/invitation-template.html`;
+const invitationTemplatePath = `${process.cwd()}/source/templates/invitation.html`;
 
 export const invitationTemplate = async (data) => {
     const html = await fsPromisify.readFile(invitationTemplatePath, "utf8");
