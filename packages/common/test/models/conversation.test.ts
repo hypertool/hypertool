e95aa01e1fd17a1f9bdfe5a624e7327f60caaa08
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-14 22:55:22 +0530

    test: added test for `page` attribute in `Conversation` model

--- packages/common/test/models/conversation.test.ts
+++ packages/common/test/models/conversation.test.ts
@@ -47,4 +47,51 @@ describe("Conversation model", function () {
             "The app attribute is required.",
         );
     });
+
+    it("should not be created when page is empty", async () => {
+        const newConversation = new ConversationModel({
+            ...conversation,
+            page: "",
+        });
+
+        await assertThrowsAsync(
+            async () => await newConversation.save(),
+            "The page attribute should have at least 1 character.",
+        );
+    });
+
+    it("should not be created when page is too long", async () => {
+        const newConversation = new ConversationModel({
+            ...conversation,
+            page: new Array(129 + 1).join("a"),
+        });
+        await assertThrowsAsync(
+            async () => newConversation.save(),
+            "The page attribute should have a maximum of 128 characters.",
+        );
+    });
+
+    it("should not be created when page is undefined", async () => {
+        const newConversation = new ConversationModel({
+            ...conversation,
+            page: undefined,
+        });
+
+        await assertThrowsAsync(
+            async () => newConversation.save(),
+            "The page attribute is required.",
+        );
+    });
+
+    it("should not be created when page is null", async () => {
+        const newConversation = new ConversationModel({
+            ...conversation,
+            page: null,
+        });
+
+        await assertThrowsAsync(
+            async () => newConversation.save(),
+            "The page attribute is required.",
+        );
+    });
 });
