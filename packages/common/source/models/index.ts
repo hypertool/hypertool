80a199e1faced63903f0ed8887d845d4080aac64
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-14 22:42:45 +0530

    feat: added and exported `Conversation` model

--- packages/common/source/models/index.ts
+++ packages/common/source/models/index.ts
@@ -7,3 +7,4 @@ export { default as QueryTemplateModel } from "./QueryTemplateModel";
 export { default as ActivityLogModel } from "./ActivityLogModel";
 export { default as DeploymentModel } from "./DeploymentModel";
 export { default as MembershipModel } from "./MembershipModel";
+export { default as ConversationModel } from "./ConversationModel";
