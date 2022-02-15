5808011c3dbd25578946369db8a066af2f96af22
Samuel Rowe <samuelrowe1999@gmail.com>
2022-02-15 13:31:49 +0530

    refactor: moved workspace layout to its own directory

---
+++ packages/console/src/layouts/workspace/index.tsx
@@ -0,0 +1,44 @@
+import type { FunctionComponent, ReactElement } from "react";
+
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import { Outlet } from "react-router-dom";
+
+import { MiniDrawer, PrimaryAppBar } from "../../components";
+
+const Root = styled("div")(({ theme }) => ({
+    backgroundColor: theme.palette.background.default,
+    minHeight: "100vh",
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+}));
+
+const Main = styled("main")(({ theme }) => ({
+    marginTop: theme.spacing(8),
+    width: "100%",
+}));
+
+const WorkspaceLayout: FunctionComponent = (): ReactElement => {
+    const [open, setOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Root>
+            <PrimaryAppBar open={open} onDrawerOpen={handleDrawerOpen} />
+            <MiniDrawer open={open} onDrawerClose={handleDrawerClose} />
+            <Main>
+                <Outlet />
+            </Main>
+        </Root>
+    );
+};
+
+export default WorkspaceLayout;
