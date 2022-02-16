f88344710891f58adb61c7ac2e51d0f512ce39a6
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-02-16 15:50:34 +0530

    test: modified tests for `page` attribute in `ConversationModel`

--- packages/common/test/models/conversation.test.ts
+++ packages/common/test/models/conversation.test.ts
@@ -48,29 +48,6 @@ describe("Conversation model", function () {
         );
     });
 
-    it("should not be created when page is empty", async () => {
-        const newConversation = new ConversationModel({
-            ...conversation,
-            page: "",
-        });
-
-        await assertThrowsAsync(
-            async () => await newConversation.save(),
-            "The page attribute should have at least 1 character.",
-        );
-    });
-
-    it("should not be created when page is too long", async () => {
-        const newConversation = new ConversationModel({
-            ...conversation,
-            page: new Array(129 + 1).join("a"),
-        });
-        await assertThrowsAsync(
-            async () => newConversation.save(),
-            "The page attribute should have a maximum of 128 characters.",
-        );
-    });
-
     it("should not be created when page is undefined", async () => {
         const newConversation = new ConversationModel({
             ...conversation,
