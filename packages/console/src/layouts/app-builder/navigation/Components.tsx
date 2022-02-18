2dbf7d15d196a0b0a1826c145cf679fb83bf645e
Joel E. Rego <joelerego@gmail.com>
2022-02-18 07:17:46 +0530

    feat: completed adding functionality to the toolbar

--- packages/console/src/layouts/app-builder/navigation/Components.tsx
+++ packages/console/src/layouts/app-builder/navigation/Components.tsx
@@ -1,25 +1,63 @@
-import { Box, Typography, Grid, Button as MaterialButton } from "@mui/material";
+import { Element, useEditor } from "@craftjs/core";
+import { Box, Grid, Button as MaterialButton, Typography } from "@mui/material";
+
+import { Button, Card, Container, Text } from "../../../nodes";
 
 const Components = () => {
+    const { connectors } = useEditor();
+
     return (
         <Box px={2} py={2}>
-            <Grid container={true} spacing={1}>
+            <Grid>
                 <Box pb={2}>
                     <Typography>Drag to add</Typography>
                 </Box>
                 <Grid container direction="column" item>
-                    <MaterialButton variant="contained">Button</MaterialButton>
+                    <MaterialButton
+                        ref={(ref) =>
+                            connectors.create(
+                                ref as any,
+                                <Button text="Click me" size="small" />,
+                            )
+                        }
+                        variant="contained"
+                    >
+                        Button
+                    </MaterialButton>
                 </Grid>
                 <Grid container direction="column" item>
-                    <MaterialButton variant="contained">Text</MaterialButton>
+                    <MaterialButton
+                        ref={(ref) =>
+                            connectors.create(
+                                ref as any,
+                                <Text text="Hi world" />,
+                            )
+                        }
+                        variant="contained"
+                    >
+                        Text
+                    </MaterialButton>
                 </Grid>
                 <Grid container direction="column" item>
-                    <MaterialButton variant="contained">
+                    <MaterialButton
+                        ref={(ref) =>
+                            connectors.create(
+                                ref as any,
+                                <Element is={Container} padding={20} canvas />,
+                            )
+                        }
+                        variant="contained"
+                    >
                         Container
                     </MaterialButton>
                 </Grid>
                 <Grid container direction="column" item>
-                    <MaterialButton variant="contained">Card</MaterialButton>
+                    <MaterialButton
+                        ref={(ref) => connectors.create(ref as any, <Card />)}
+                        variant="contained"
+                    >
+                        Card
+                    </MaterialButton>
                 </Grid>
             </Grid>
         </Box>
--- packages/console/src/nodes/Card.tsx
+++ packages/console/src/nodes/Card.tsx
@@ -1,22 +1,67 @@
-import { Text } from "./Text";
+import { Element, useNode } from "@craftjs/core";
+
 import { Button } from "./Button";
 import { Container } from "./Container";
+import { Text } from "./Text";
+
+export const CardTop = ({ children }: any) => {
+    const {
+        connectors: { connect },
+    } = useNode();
+    return (
+        <div ref={connect as any} className="text-only">
+            {children}
+        </div>
+    );
+};
+
+CardTop.craft = {
+    rules: {
+        canMoveIn: (incomingNodes: any) =>
+            incomingNodes.every(
+                (incomingNode: {
+                    data: { type: ({ text }: any) => JSX.Element };
+                }) => incomingNode.data.type === Text,
+            ),
+    },
+};
+
+export const CardBottom = ({ children }: any) => {
+    const {
+        connectors: { connect },
+    } = useNode();
+    return <div ref={connect as any}>{children}</div>;
+};
+
+CardBottom.craft = {
+    rules: {
+        // Only accept Buttons
+        canMoveIn: (incomingNodes: any) =>
+            incomingNodes.every(
+                (incomingNode: {
+                    data: {
+                        type: ({
+                            size,
+                            variant,
+                            color,
+                            children,
+                        }: any) => JSX.Element;
+                    };
+                }) => incomingNode.data.type === Button,
+            ),
+    },
+};
 
 export const Card = ({ background, padding = 20 }: any) => {
     return (
         <Container background={background} padding={padding}>
-            <div className="text-only">
+            <Element id="text" is={CardTop} canvas={true}>
                 <Text text="Title" fontSize={20} />
                 <Text text="Subtitle" fontSize={15} />
-            </div>
-            <div className="buttons-only">
-                <Button
-                    size="small"
-                    text="Learn more"
-                    variant="contained"
-                    color="primary"
-                />
-            </div>
+            </Element>
+            <Element id="buttons" is={CardBottom} canvas={true}>
+                <Button size="small" text="Learn more" />
+            </Element>
         </Container>
     );
 };
