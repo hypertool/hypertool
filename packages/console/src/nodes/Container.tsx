5d363adbe385bbb631d3ebd5169038747cd6ae43
Joel E. Rego <joelerego@gmail.com>
2022-02-18 07:38:34 +0530

    feat: created a related component that shares `Container` node

--- packages/console/src/nodes/Container.tsx
+++ packages/console/src/nodes/Container.tsx
@@ -1,5 +1,6 @@
 import { useNode } from "@craftjs/core";
-import { Paper } from "@mui/material";
+import { FormControl, FormLabel, Slider, Paper } from "@mui/material";
+import ColorPicker from "material-ui-color-picker";
 
 export const Container = ({ background, padding = 0, children }: any) => {
     const {
@@ -8,10 +9,54 @@ export const Container = ({ background, padding = 0, children }: any) => {
 
     return (
         <Paper
-            ref={(ref) => connect(drag(ref as any))}
-            style={{ background, padding: `${padding}px` }}
-        >
+            ref={(ref: any) => connect(drag(ref as any))}
+            style={{ background, padding: `${padding}px` }}>
             {children}
         </Paper>
     );
+};
+
+export const ContainerSettings = () => {
+    const {
+        background,
+        padding,
+        actions: { setProp },
+    } = useNode((node) => ({
+        background: node.data.props.background,
+        padding: node.data.props.padding,
+    }));
+    return (
+        <div>
+            <FormControl fullWidth={true} margin="normal" component="fieldset">
+                <FormLabel component="legend">Background</FormLabel>
+                <ColorPicker
+                    defaultValue={background || "#000"}
+                    onChange={(color: any) => {
+                        setProp(
+                            (props: { background: any }) =>
+                                (props.background = color),
+                        );
+                    }}
+                />
+            </FormControl>
+            <FormControl fullWidth={true} margin="normal" component="fieldset">
+                <FormLabel component="legend">Padding</FormLabel>
+                <Slider
+                    defaultValue={padding}
+                    onChange={(_, value) =>
+                        setProp(
+                            (props: { padding: number | number[] }) =>
+                                (props.padding = value),
+                        )
+                    }
+                />
+            </FormControl>
+        </div>
+    );
+};
+
+Container.craft = {
+    related: {
+        settings: ContainerSettings,
+    },
 };
