27e10d49f03d51af06f4a63a4d042c4002a51f96
Joel E. Rego <joelerego@gmail.com>
2022-02-18 07:40:02 +0530

    feat: made `Card` and `Container` related components

--- packages/console/src/nodes/Card.tsx
+++ packages/console/src/nodes/Card.tsx
@@ -1,7 +1,7 @@
 import { Element, useNode } from "@craftjs/core";
 
 import { Button } from "./Button";
-import { Container } from "./Container";
+import { Container, ContainerSettings } from "./Container";
 import { Text } from "./Text";
 
 export const CardTop = ({ children }: any) => {
@@ -64,4 +64,10 @@ export const Card = ({ background, padding = 20 }: any) => {
             </Element>
         </Container>
     );
+};
+
+Card.craft = {
+    related: {
+        settings: ContainerSettings,
+    },
 };
