3c61901409dfaf7d083cf7e72fcaa6c350a0ee9b
Samuel Rowe <samuelrowe1999@gmail.com>
2022-02-28 16:49:49 +0530

    feat: created `ExplorerAccordions` component

---
+++ packages/console/src/screens/app-builder/panels/explorer/ExplorerAccordions.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode } from "react";
+
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Icon,
+    Typography,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+import Layers from "./Layers";
+
+const StyledAccordion = styled(Accordion)({});
+
+const AccordionTitle = styled(Typography)({
+    fontSize: 14,
+});
+
+const StyledAccordionDetails = styled(AccordionDetails)({
+    padding: 0,
+});
+
+const ExplorerAccordions = () => {
+    const renderAccordion = (id: string, title: string, node: ReactNode) => (
+        <StyledAccordion key={id} disableGutters={true} square={true}>
+            <AccordionSummary
+                expandIcon={<Icon fontSize="small">expand_more</Icon>}
+                id={`${id}-header`}>
+                <AccordionTitle>{title}</AccordionTitle>
+            </AccordionSummary>
+            <StyledAccordionDetails>{node}</StyledAccordionDetails>
+        </StyledAccordion>
+    );
+
+    return <div>{renderAccordion("layers", "Layers", <Layers />)}</div>;
+};
+
+export default ExplorerAccordions;
