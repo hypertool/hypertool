a587ba829db26cfd651b0831d5bdcb9067358e74
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-01 09:41:51 +0530

    feat: created the add queries button

--- packages/console/src/screens/app-builder/panels/explorer/Queries.tsx
+++ packages/console/src/screens/app-builder/panels/explorer/Queries.tsx
@@ -2,6 +2,7 @@ import type { FunctionComponent, ReactElement } from "react";
 
 import {
     Avatar,
+    Button,
     Icon,
     IconButton,
     List,
@@ -9,13 +10,22 @@ import {
     ListItemAvatar,
     ListItemText,
 } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const Actions = styled("div")(({ theme }) => ({
+    display: "flex",
+    flexDirection: "row",
+    padding: `${theme.spacing(1)} ${theme.spacing(2)} ${theme.spacing(
+        2,
+    )} ${theme.spacing(2)}`,
+}));
 
 const Queries: FunctionComponent = (): ReactElement => {
     const renderQuery = (title: string) => (
         <ListItem
             key={title}
             secondaryAction={
-                <IconButton edge="end" aria-label="delete">
+                <IconButton edge="end">
                     <Icon fontSize="small">delete</Icon>
                 </IconButton>
             }>
@@ -31,6 +41,16 @@ const Queries: FunctionComponent = (): ReactElement => {
     return (
         <div>
             <List dense={true}>{["magento", "trell"].map(renderQuery)}</List>
+            <Actions>
+                <Button
+                    size="small"
+                    fullWidth={true}
+                    variant="outlined"
+                    color="primary"
+                    endIcon={<Icon>add</Icon>}>
+                    Create New Query
+                </Button>
+            </Actions>
         </div>
     );
 };
