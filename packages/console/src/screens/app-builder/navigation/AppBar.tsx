12351e175f9b200740f5c29691e936fb5f094487
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-01 13:12:23 +0530

    feat: added tabs to app bar in app builder screen

--- packages/console/src/screens/app-builder/navigation/AppBar.tsx
+++ packages/console/src/screens/app-builder/navigation/AppBar.tsx
@@ -1,34 +1,34 @@
 import type { FunctionComponent, ReactElement } from "react";
 
 import type { AppBarProps } from "@mui/material";
-import { AppBar as MuiAppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar as MuiAppBar, Toolbar } from "@mui/material";
 import { styled } from "@mui/material/styles";
+
+import BuilderTabs from "./BuilderTabs";
 
 interface PrimaryAppBarProps extends AppBarProps {
     open?: boolean;
 }
 
-const DecoratedAppBar = styled(MuiAppBar, {
+const StyledAppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== "open",
-})<PrimaryAppBarProps>(({ theme, open }) => ({
+})<PrimaryAppBarProps>(({ theme, open }: any) => ({
     transition: theme.transitions.create(["width", "margin"], {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
     }),
     background: (theme.palette.background as any).main,
     zIndex: theme.zIndex.drawer + 1,
+    height: 48,
+    width: `calc(100% - ${open ? 304 : 56}px)`,
+    padding: `0px ${theme.spacing(2)}`,
+    "& .MuiToolbar-root": {
+        height: 48,
+        minHeight: 48,
+    },
 }));
 
-const Logo = styled("img")(({ theme }) => ({
-    width: 32,
-    height: "auto",
-    marginRight: theme.spacing(1),
-}));
-
-const ToolName = styled(Typography)(({ theme }) => ({
-    fontSize: 18,
-    fontWeight: "bold",
-}));
+const StyledToolbar = styled(Toolbar)({});
 
 interface Props {
     open: boolean;
@@ -37,14 +37,11 @@ interface Props {
 const AppBar: FunctionComponent<Props> = (props: Props): ReactElement => {
     const { open } = props;
     return (
-        <DecoratedAppBar open={open}>
-            <Toolbar>
-                <Logo src="https://res.cloudinary.com/hypertool/image/upload/v1642502111/hypertool-starter/hypertool-logo_xvqljy.png" />
-                <ToolName variant="h6" sx={{ flexGrow: 1 }}>
-                    Hypertool
-                </ToolName>
-            </Toolbar>
-        </DecoratedAppBar>
+        <StyledAppBar open={open}>
+            <StyledToolbar>
+                <BuilderTabs />
+            </StyledToolbar>
+        </StyledAppBar>
     );
 };
 
