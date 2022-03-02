50fe8d8559f17d677e9af2a77c7c36fea5efc360
Pranjal Chourasia <pranjalchourasia2915@gmail.com>
2022-03-02 23:56:24 +0530

    feat: added types for `Typography` component

--- packages/console/src/types/index.tsx
+++ packages/console/src/types/index.tsx
@@ -146,6 +146,29 @@ export interface ITab {
     bundle?: any;
 }
 
+export type TypoGraphyVariant =
+    | "body1"
+    | "body2"
+    | "button"
+    | "caption"
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "inherit"
+    | "overline"
+    | "subtitle1"
+    | "subtitle2";
+
+export type TypographyAlign =
+    | "center"
+    | "inherit"
+    | "justify"
+    | "left"
+    | "right";
+
 export interface IBuilderActionsContext {
     createNewTab: (title: string, type: TTabType) => void;
     tabs: ITab[];
