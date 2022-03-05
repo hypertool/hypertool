b12f91485cc630b22aa639d5b6b91bd9dc50d750
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-05 08:41:55 +0530

    feat: created `Controller` Mongoose model

---
+++ packages/common/source/models/controller-model.ts
@@ -0,0 +1,65 @@
+import { Schema, model } from "mongoose";
+import paginate from "mongoose-paginate-v2";
+
+import type { IController } from "../types";
+import { controllerStatuses } from "../utils/constants";
+
+const patchSchema = new Schema({
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        immutable: true,
+    },
+    content: {
+        type: String,
+        required: true,
+        maxlength: 24 * 1024,
+        immutable: true,
+    },
+    createdAt: { type: Date, immutable: true, default: () => new Date() },
+});
+
+const controllerSchema = new Schema(
+    {
+        /* An identifier that points to the User who created the controller. */
+        creator: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        /**
+         * List of patches that describe the content of the controller.
+         *
+         * We use the patch format that Git uses to store controller versions
+         * in the database. The `diff` package is used to achieve this.
+         * We do not keep the "plain" controller source code in the database.
+         * When a file is loaded by the console, we apply the patches to generate
+         * the latest copy of the file.
+         *
+         * The console implements three techniques for saving files, preventing
+         * data loss.
+         * 1. Save button that the user can trigger manually -- data stored as patches
+         * 2. Autosave triggered every 1s (configurable by the user) -- data stored as patches
+         * 3. Parallel writes to local storage as the user types -- data stored as plain text
+         */
+        patches: {
+            type: [patchSchema],
+            default: [],
+        },
+        /*
+         * An enumeration of string values that describes the status of the
+         * controller.
+         */
+        status: {
+            type: String,
+            enum: controllerStatuses,
+            required: true,
+        },
+        createdAt: { type: Date, immutable: true },
+    },
+    { timestamps: true },
+);
+
+controllerSchema.plugin(paginate);
+export default model<IController>("Controller", controllerSchema);
