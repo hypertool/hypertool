eb70bbf478c98019ba9ee3ee4bd8fe3d7306c12e
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-05 20:32:38 +0530

    feat: created `create` controller for controller model

--- packages/api/source/controllers/controller-controllers.ts
+++ packages/api/source/controllers/controller-controllers.ts
@@ -1,4 +1,17 @@
 import type { IController, IExternalController } from "@hypertool/common";
+import { BadRequestError, ControllerModel, constants } from "@hypertool/common";
+
+import joi from "joi";
+
+const createSchema = joi.object({
+    creator: joi.string().regex(constants.identifierPattern),
+    patches: joi.array().items(
+        joi.object({
+            author: joi.string().regex(constants.identifierPattern),
+            content: joi.string().allow(""),
+        }),
+    ),
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const toExternal = (controller: IController): IExternalController => {
@@ -23,4 +36,31 @@ const toExternal = (controller: IController): IExternalController => {
         createdAt,
         updatedAt,
     };
+};
+
+export const create = async (
+    context: any,
+    attributes: any,
+): Promise<IExternalController> => {
+    const { error, value } = createSchema.validate(attributes, {
+        stripUnknown: true,
+    });
+
+    if (error) {
+        throw new BadRequestError(error.message);
+    }
+
+    /*
+     * TODO: Add `controller` to `app.controllers`
+     * TODO: Check if value.creator is correct.
+     * TODO: Check if value.name is unique across the organization and matches
+     * the identifier regex.
+     */
+    const newController = new ControllerModel({
+        ...value,
+        status: "created",
+    });
+    await newController.save();
+
+    return toExternal(newController);
 };
