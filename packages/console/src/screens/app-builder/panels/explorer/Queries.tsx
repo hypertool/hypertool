050124241173410ab453eea229db50c7f489bd7c
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-08 16:45:11 +0530

    feat: updated queries accordion to open edit query tab

--- packages/console/src/screens/app-builder/panels/explorer/Queries.tsx
+++ packages/console/src/screens/app-builder/panels/explorer/Queries.tsx
@@ -13,6 +13,8 @@ import {
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
+import { gql, useQuery } from "@apollo/client";
+
 import { BuilderActionsContext } from "../../../../contexts";
 
 const Actions = styled("div")(({ theme }) => ({
@@ -23,34 +25,59 @@ const Actions = styled("div")(({ theme }) => ({
     )} ${theme.spacing(2)}`,
 }));
 
+const GET_QUERY_TEMPLATES = gql`
+    query GetQueryTemplates($app: ID!, $page: Int, $limit: Int) {
+        getQueryTemplates(app: $app, page: $page, limit: $limit) {
+            totalPages
+            records {
+                id
+                name
+            }
+        }
+    }
+`;
+
 const Queries: FunctionComponent = (): ReactElement => {
     const { createTab } = useContext(BuilderActionsContext);
+    const { data } = useQuery(GET_QUERY_TEMPLATES, {
+        variables: {
+            page: 0,
+            limit: 20,
+            app: "61c93a931da4a79d3a109947",
+        },
+    });
+    const { records } = data?.getQueryTemplates || { records: [] };
 
     const handleNewQuery = useCallback(() => {
         createTab("new-query");
     }, [createTab]);
 
-    const renderQuery = (title: string) => (
+    const handleEditQuery = (queryId: string) => () => {
+        createTab("edit-query", { queryId });
+    };
+
+    const renderQuery = (record: any) => (
         <ListItem
-            key={title}
+            key={record.id}
             secondaryAction={
                 <IconButton edge="end">
                     <Icon fontSize="small">delete</Icon>
                 </IconButton>
             }
+            onClick={handleEditQuery(record.id)}
         >
             <ListItemAvatar>
                 <Avatar sx={{ width: 28, height: 28 }}>
                     <Icon fontSize="small">category</Icon>
                 </Avatar>
             </ListItemAvatar>
-            <ListItemText primary={title} />
+            <ListItemText primary={record.name} />
         </ListItem>
     );
 
     return (
         <div>
-            <List dense={true}>{["magento", "trell"].map(renderQuery)}</List>
+            <List dense={true}>{records.map(renderQuery)}</List>
             <Actions>
                 <Button
                     size="small"
