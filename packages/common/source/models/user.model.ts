d83591e322a27fdb78a380305b8e4f5e268f655a
Joel E. Rego <joelerego@gmail.com>
2022-03-09 03:02:17 +0530

    feat: modified the `user` model for simpler team management

--- packages/common/source/models/user.model.ts
+++ packages/common/source/models/user.model.ts
@@ -88,19 +88,6 @@ const userSchema = new Schema(
             enum: userStatuses,
             default: "active",
         },
-        role: {
-            type: String,
-            enum: userRoles,
-            required: true,
-        },
-        groups: {
-            type: [
-                {
-                    type: Schema.Types.ObjectId,
-                    ref: "Group",
-                },
-            ],
-        },
     },
     {
         timestamps: true,
