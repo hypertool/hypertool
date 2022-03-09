18c284be5fc9b4c52d0484aab9bca91381b57905
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-09 11:48:18 +0530

    feat: created `useReplaceTab` hook

--- packages/console/src/hooks/index.ts
+++ packages/console/src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useQueryParams } from "./useQueryParams";
 export { default as useArtifactReference } from "./useArtifactReference";
 export { default as useInflateArtifacts } from "./useInflateArtifacts";
 export { default as useArtifactsArray } from "./useArtifactsArray";
+export { default as useReplaceTab } from "./useReplaceTab";
