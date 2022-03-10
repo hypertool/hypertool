bd89441ae04cf7bfd24f6331914925612cfbc250
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-10 15:33:24 +0530

    feat: forked `@craftjs/utils`

---
+++ packages/console/src/craft-utils/EventHandlers/EventHandlers.ts
@@ -0,0 +1,182 @@
+import { ConnectorRegistry } from "./ConnectorRegistry";
+import {
+    Connector,
+    ConnectorsUsage,
+    CraftDOMEvent,
+    CraftEventListener,
+    EventHandlerConnectors,
+    EventHandlerUpdates,
+} from "./interfaces";
+import { isEventBlockedByDescendant } from "./isEventBlockedByDescendant";
+
+export abstract class EventHandlers<O extends Record<string, any> = any> {
+    options: O;
+
+    private registry: ConnectorRegistry = new ConnectorRegistry();
+    private subscribers: Set<(msg: EventHandlerUpdates) => void> = new Set();
+
+    onEnable?(): void;
+    onDisable?(): void;
+
+    constructor(options?: O) {
+        this.options = options as any;
+    }
+
+    listen(cb: (msg: EventHandlerUpdates) => void) {
+        this.subscribers.add(cb);
+        return () => this.subscribers.delete(cb);
+    }
+
+    disable() {
+        if (this.onDisable) {
+            this.onDisable();
+        }
+
+        this.registry.disable();
+
+        this.subscribers.forEach((listener) => {
+            listener(EventHandlerUpdates.HandlerDisabled);
+        });
+    }
+
+    enable() {
+        if (this.onEnable) {
+            this.onEnable();
+        }
+
+        this.registry.enable();
+
+        this.subscribers.forEach((listener) => {
+            listener(EventHandlerUpdates.HandlerEnabled);
+        });
+    }
+
+    cleanup() {
+        this.disable();
+        this.subscribers.clear();
+        this.registry.clear();
+    }
+
+    addCraftEventListener<K extends keyof HTMLElementEventMap>(
+        el: HTMLElement,
+        eventName: K,
+        listener: CraftEventListener<K>,
+        options?: boolean | AddEventListenerOptions,
+    ) {
+        const bindedListener = (e: CraftDOMEvent<HTMLElementEventMap[K]>) => {
+            if (!isEventBlockedByDescendant(e, eventName, el)) {
+                e.craft.stopPropagation = () => {
+                    if (!e.craft.blockedEvents[eventName]) {
+                        e.craft.blockedEvents[eventName] = [];
+                    }
+
+                    e.craft.blockedEvents[eventName].push(el);
+                };
+
+                listener(e);
+            }
+        };
+
+        el.addEventListener(eventName, bindedListener as any, options);
+
+        return () =>
+            el.removeEventListener(eventName, bindedListener as any, options);
+    }
+
+    // Defines the connectors and their logic
+    abstract handlers(): Record<
+        string,
+        (el: HTMLElement, ...args: any[]) => any
+    >;
+
+    /**
+     * Creates a record of chainable connectors and tracks their usages
+     */
+    createConnectorsUsage(): ConnectorsUsage<this> {
+        const handlers = this.handlers();
+
+        /*
+         * Track all active connector ids here
+         * This is so we can return a cleanup method below so the callee can programmatically cleanup all connectors
+         */
+        const activeConnectorIds: Set<string> = new Set();
+
+        const connectors = Object.entries(handlers).reduce<
+            Record<string, Connector>
+        >(
+            (accum, [name, handler]) => ({
+                ...accum,
+                [name]: (el, required, options) => {
+                    const connector = this.registry.register(el, {
+                        required,
+                        name,
+                        options,
+                        connector: handler,
+                    });
+
+                    activeConnectorIds.add(connector?.id as any);
+
+                    return el;
+                },
+            }),
+            {},
+        ) as any;
+
+        return {
+            connectors,
+            cleanup: () => {
+                activeConnectorIds.forEach((connectorId) =>
+                    this.registry.remove(connectorId),
+                );
+            },
+        };
+    }
+
+    derive<C extends EventHandlers>(
+        type: {
+            new (...args: any[]): C;
+        },
+        opts: C["options"],
+    ) {
+        return new type(this, opts);
+    }
+
+    // This method allows us to execute multiple connectors and returns a single cleanup method for all of them
+    protected createProxyHandlers<H extends EventHandlers>(
+        instance: H,
+        cb: (connectors: EventHandlerConnectors<H>) => void,
+    ) {
+        const connectorsToCleanup: any[] = [];
+        const handlers = instance.handlers();
+
+        const proxiedHandlers = new Proxy(handlers, {
+            get: (target, key: any, receiver) => {
+                if (key in handlers === false) {
+                    return Reflect.get(target, key, receiver);
+                }
+
+                return (el: any, ...args: any[]) => {
+                    const cleanup = handlers[key](el, ...args);
+                    if (!cleanup) {
+                        return;
+                    }
+
+                    connectorsToCleanup.push(cleanup);
+                };
+            },
+        });
+
+        cb(proxiedHandlers as any);
+
+        return () => {
+            connectorsToCleanup.forEach((cleanup) => {
+                cleanup();
+            });
+        };
+    }
+
+    // This lets us to execute and cleanup sibling connectors
+    reflect(cb: (connectors: EventHandlerConnectors<this>) => void) {
+        return this.createProxyHandlers(this, cb);
+    }
+}
---
+++ packages/console/src/craft-utils/History.ts
@@ -0,0 +1,119 @@
+import { Patch, applyPatches } from "immer";
+
+type Timeline = Array<{
+    patches: Patch[];
+    inversePatches: Patch[];
+    timestamp: number;
+}>;
+
+export const HISTORY_ACTIONS = {
+    UNDO: "HISTORY_UNDO",
+    REDO: "HISTORY_REDO",
+    THROTTLE: "HISTORY_THROTTLE",
+    IGNORE: "HISTORY_IGNORE",
+    MERGE: "HISTORY_MERGE",
+    CLEAR: "HISTORY_CLEAR",
+};
+
+export class History {
+    timeline: Timeline = [];
+    pointer = -1;
+
+    add(patches: Patch[], inversePatches: Patch[]) {
+        if (patches.length === 0 && inversePatches.length === 0) {
+            return;
+        }
+
+        this.pointer = this.pointer + 1;
+        this.timeline.length = this.pointer;
+        this.timeline[this.pointer] = {
+            patches,
+            inversePatches,
+            timestamp: Date.now(),
+        };
+    }
+
+    throttleAdd(patches: Patch[], inversePatches: Patch[], throttleRate = 500) {
+        if (patches.length === 0 && inversePatches.length === 0) {
+            return;
+        }
+
+        if (this.timeline.length && this.pointer >= 0) {
+            const {
+                patches: currPatches,
+                inversePatches: currInversePatches,
+                timestamp,
+            } = this.timeline[this.pointer];
+
+            const now = new Date();
+            const diff = now.getTime() - timestamp;
+
+            if (diff < throttleRate) {
+                this.timeline[this.pointer] = {
+                    timestamp,
+                    patches: [...currPatches, ...patches],
+                    inversePatches: [...inversePatches, ...currInversePatches],
+                };
+                return;
+            }
+        }
+
+        this.add(patches, inversePatches);
+    }
+
+    merge(patches: Patch[], inversePatches: Patch[]) {
+        if (patches.length === 0 && inversePatches.length === 0) {
+            return;
+        }
+
+        if (this.timeline.length && this.pointer >= 0) {
+            const {
+                patches: currPatches,
+                inversePatches: currInversePatches,
+                timestamp,
+            } = this.timeline[this.pointer];
+
+            this.timeline[this.pointer] = {
+                timestamp,
+                patches: [...currPatches, ...patches],
+                inversePatches: [...inversePatches, ...currInversePatches],
+            };
+            return;
+        }
+
+        this.add(patches, inversePatches);
+    }
+
+    clear() {
+        this.timeline = [];
+        this.pointer = -1;
+    }
+
+    canUndo() {
+        return this.pointer >= 0;
+    }
+
+    canRedo() {
+        return this.pointer < this.timeline.length - 1;
+    }
+
+    undo(state: any) {
+        if (!this.canUndo()) {
+            return;
+        }
+
+        const { inversePatches } = this.timeline[this.pointer];
+        this.pointer = this.pointer - 1;
+        return applyPatches(state, inversePatches);
+    }
+
+    redo(state: any) {
+        if (!this.canRedo()) {
+            return;
+        }
+
+        this.pointer = this.pointer + 1;
+        const { patches } = this.timeline[this.pointer];
+        return applyPatches(state, patches);
+    }
+}
