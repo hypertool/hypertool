7f6b87d4cc0b29b40693e5a2f74ac38f68e03905
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-19 12:01:11 +0530

    feat: renamed `useTabContext` to `useTab`

--- packages/console/src/hooks/index.ts
+++ packages/console/src/hooks/index.ts
@@ -3,7 +3,7 @@ export { default as useArtifactReference } from "./useArtifactReference";
 export { default as useInflateArtifacts } from "./useInflateArtifacts";
 export { default as useArtifactsArray } from "./useArtifactsArray";
 export { default as useReplaceTab } from "./useReplaceTab";
-export { default as useTabContext } from "./useTabContext";
+export { default as useTab } from "./useTab";
 export { default as useUpdateTabTitle } from "./useUpdateTabTitle";
 export { default as useTabBundle } from "./useTabBundle";
 export { default as useInterval } from "./useInterval";
