6c4d4900ce58ef700e4291e0be3c2de1136504a5
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-27 17:56:03 +0530

    feat: created `View` component

---
+++ src/components/View.tsx
@@ -0,0 +1,19 @@
+import type { FunctionComponent, ReactElement } from "react";
+import { INode } from "../types";
+
+export interface IProps {
+  node: INode;
+}
+
+const View: FunctionComponent<IProps> = (props: IProps): ReactElement => {
+  const { node } = props;
+  return (
+    <div>
+      {node.children.map((node) => (
+        <View key={node.id} node={node} />
+      ))}
+    </div>
+  );
+};
+
+export default View;
