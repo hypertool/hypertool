ff2495e17e6c0639cd343242df2f222468060410
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-27 17:35:22 +0530

    feat: created `INode`, `ICraftNode`, and `TCraftNodeKey` types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,47 @@
 export interface IScreen {
-    id: string;
-    app: IApp;
-    name: string;
-    title: string;
-    description: string;
-    slug: string;
-    content: string;
-    status: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  app: IApp;
+  name: string;
+  title: string;
+  description: string;
+  slug: string;
+  content: string;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface IApp {
-    id: string;
-    name: string;
-    title: string;
-    slug: string;
-    description: string;
-    screens: IScreen[];
-    status: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  name: string;
+  title: string;
+  slug: string;
+  description: string;
+  screens: IScreen[];
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export type TAppContext = IApp;
+
+export interface INode {
+  id: string;
+  children: INode[];
+}
+
+export interface ICraftNode {
+  custom: {
+    [key: string]: any;
+  };
+  nodes: string[];
+  props: {
+    [key: string]: any;
+  };
+  type: {
+    resolvedName: string;
+  };
+  parent?: string;
+}
+
+export type TCraftNodeKey = keyof ICraftNode;
