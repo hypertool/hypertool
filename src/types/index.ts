8605ce21d5ac737bdb88fa0c3ad496faaff2a221
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-27 09:29:20 +0530

    feat: created `IScreen` interface and updated `IApp`

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,26 @@
+export interface IScreen {
+    id: string;
+    app: IApp;
+    name: string;
+    title: string;
+    description: string;
+    slug: string;
+    content: string;
+    status: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
 export interface IApp {
     id: string;
+    name: string;
+    title: string;
+    slug: string;
+    description: string;
+    screens: IScreen[];
+    status: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
 
 export type TAppContext = IApp;
