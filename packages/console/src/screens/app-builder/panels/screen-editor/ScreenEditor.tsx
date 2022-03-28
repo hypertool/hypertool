e6516b0ec1f935e98143e3d41f7aaa47275ba38f
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-28 15:30:56 +0530

    feat: added save button for screen editor

--- packages/console/src/screens/app-builder/panels/screen-editor/ScreenEditor.tsx
+++ packages/console/src/screens/app-builder/panels/screen-editor/ScreenEditor.tsx
@@ -1,9 +1,9 @@
-import type { FunctionComponent, ReactElement } from "react";
+import { FunctionComponent, ReactElement, useCallback } from "react";
 
-import { FormControlLabel } from "@mui/material";
+import { Button, FormControlLabel } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 import * as yup from "yup";
 import { Formik } from "formik";
@@ -16,7 +16,8 @@ const Root = styled("div")(({ theme }) => ({
     padding: theme.spacing(2),
     display: "flex",
     flexDirection: "column",
-    justifyContent: "flex-start",
+    justifyContent: "space-between",
+    minHeight: "calc(100vh - 96px)",
 }));
 
 const NameTextField = styled(TextField)(({ theme }) => ({
@@ -30,6 +31,14 @@ const TitleTextField = styled(TextField)(({ theme }) => ({
 const SlugTextField = styled(TextField)(({ theme }) => ({
     marginTop: theme.spacing(3),
 }));
+
+const FormBody = styled("div")({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+});
+
+const Actions = styled("div")({});
 
 const validationSchema = yup.object({
     name: yup
@@ -64,6 +73,24 @@ const GET_SCREEN = gql`
     }
 `;
 
+const UPDATE_SCREEN = gql`
+    mutation UpdateScreen(
+        $screenId: ID!
+        $name: String!
+        $title: String!
+        $slug: String!
+    ) {
+        updateScreen(
+            screenId: $screenId
+            name: $name
+            title: $title
+            slug: $slug
+        ) {
+            id
+        }
+    }
+`;
+
 const ScreenEditor: FunctionComponent<IProps> = (
     props: IProps,
 ): ReactElement => {
@@ -76,6 +103,25 @@ const ScreenEditor: FunctionComponent<IProps> = (
         },
         notifyOnNetworkStatusChange: true,
     });
+    const [
+        updateScreen,
+        /*
+         * {
+         *     loading: updatingScreen,
+         *     data: updatedScreen,
+         *     error: updateScreenError,
+         * },
+         */
+    ] = useMutation(UPDATE_SCREEN);
+
+    const handleSave = useCallback((values: any) => {
+        updateScreen({
+            variables: {
+                screenId,
+                ...values,
+            },
+        });
+    }, []);
 
     const {
         name = "",
@@ -94,58 +140,72 @@ const ScreenEditor: FunctionComponent<IProps> = (
                     controller,
                 }}
                 enableReinitialize={true}
-                onSubmit={async () => null}
+                onSubmit={handleSave}
                 validationSchema={validationSchema}
             >
-                <>
-                    <NameTextField
-                        id="name"
-                        name="name"
-                        label="Name"
-                        type="text"
-                        fullWidth={true}
-                        variant="outlined"
-                        required={true}
-                        help=""
-                        size="small"
-                    />
-                    <SlugTextField
-                        id="controller"
-                        name="controller"
-                        label="Controller"
-                        type="text"
-                        fullWidth={true}
-                        variant="outlined"
-                        required={true}
-                        help=""
-                        size="small"
-                        inputProps={{
-                            readOnly: true,
-                        }}
-                    />
-                    <TitleTextField
-                        id="title"
-                        name="title"
-                        label="Title"
-                        type="text"
-                        fullWidth={true}
-                        variant="outlined"
-                        required={true}
-                        help=""
-                        size="small"
-                    />
-                    <SlugTextField
-                        id="slug"
-                        name="slug"
-                        label="Slug"
-                        type="text"
-                        fullWidth={true}
-                        variant="outlined"
-                        required={true}
-                        help=""
-                        size="small"
-                    />
-                </>
+                {(formik) => (
+                    <>
+                        <FormBody>
+                            <NameTextField
+                                id="name"
+                                name="name"
+                                label="Name"
+                                type="text"
+                                fullWidth={true}
+                                variant="outlined"
+                                required={true}
+                                help=""
+                                size="small"
+                            />
+                            <SlugTextField
+                                id="controller"
+                                name="controller"
+                                label="Controller"
+                                type="text"
+                                fullWidth={true}
+                                variant="outlined"
+                                required={true}
+                                help=""
+                                size="small"
+                                inputProps={{
+                                    readOnly: true,
+                                }}
+                            />
+                            <TitleTextField
+                                id="title"
+                                name="title"
+                                label="Title"
+                                type="text"
+                                fullWidth={true}
+                                variant="outlined"
+                                required={true}
+                                help=""
+                                size="small"
+                            />
+                            <SlugTextField
+                                id="slug"
+                                name="slug"
+                                label="Slug"
+                                type="text"
+                                fullWidth={true}
+                                variant="outlined"
+                                required={true}
+                                help=""
+                                size="small"
+                            />
+                        </FormBody>
+                        <Actions>
+                            <Button
+                                variant="contained"
+                                fullWidth={true}
+                                type="submit"
+                                onClick={formik.submitForm}
+                            >
+                                Save
+                            </Button>
+                        </Actions>
+                    </>
+                )}
             </Formik>
         </Root>
     );
