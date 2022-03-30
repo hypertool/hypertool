ba685022eb520fe0fb9c1740fe8b5988a14c7ce0
Samuel Rowe <samuelrowe1999@gmail.com>
2022-03-30 07:56:55 +0530

    feat: created `Node` component

---
+++ packages/console/src/nodes/Node.tsx
@@ -0,0 +1,57 @@
+import type { FunctionComponent, ReactElement } from "react";
+
+import { styled } from "@mui/material/styles";
+
+import { useNode } from "../craft";
+
+export interface IProps {
+    backgroundColor?: string;
+    width?: string;
+    height?: string;
+    children: ReactElement;
+}
+
+interface IRootProps {
+    selected: boolean;
+    hovered: boolean;
+}
+
+const Root = styled("div", {
+    shouldForwardProp: (prop: string) =>
+        !["hovered", "selected"].includes(prop),
+})<IRootProps>(({ theme, hovered, selected }) => ({
+    border:
+        hovered || selected
+            ? `1.5px solid ${theme.palette.primary.light}`
+            : undefined,
+    width: "fit-content",
+    height: "fit-content",
+
+    "& > *": {
+        pointerEvents: "none",
+    },
+}));
+
+const Node: FunctionComponent<IProps> = (props: IProps): ReactElement => {
+    const { children } = props;
+    const {
+        connectors: { connect, drag },
+        hovered,
+        selected,
+    } = useNode((state) => ({
+        hovered: state.events.hovered,
+        selected: state.events.selected,
+    }));
+
+    return (
+        <Root
+            ref={(ref) => connect(drag(ref))}
+            selected={selected}
+            hovered={hovered}
+        >
+            {children}
+        </Root>
+    );
+};
+
+export default Node;
