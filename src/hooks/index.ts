b7f5049b315cfa87b711acf5e5308d3ebbc52fb2
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-01 12:19:11 +0530

    feat: created `useCallbackSymbol` hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useFetchApp } from "./useFetchApp";
 export { default as useApp } from "./useApp";
 export { default as useControllers } from "./useControllers";
 export { default as useSymbolReference } from "./useSymbolReference";
+export { default as useCallbackSymbol } from "./useCallbackSymbol";
---
+++ src/hooks/useCallbackSymbol.ts
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { ScreenContext } from "../contexts";
+import { ISymbolReference } from "../types";
+import useSymbolReference from "./useSymbolReference";
+
+const useCallbackSymbol = <R>(reference?: ISymbolReference | null) => {
+  const symbol = useSymbolReference<R>(reference);
+  const context = useContext(ScreenContext);
+
+  /* Check `!reference` only to satisfy TypeScript, because
+   * `symbol` will be null if `reference` is null.
+   */
+  if (!reference || !symbol) {
+    return () => {};
+  }
+
+  if (!(symbol instanceof Function)) {
+    throw new Error(
+      `The referenced symbol "${reference.moduleId}.${reference.target}" is not a function.`
+    );
+  }
+
+  return ((...values: any[]) => symbol(context, ...values)) as unknown as R;
+};
+
+export default useCallbackSymbol;
