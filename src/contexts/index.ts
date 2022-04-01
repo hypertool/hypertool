295e73fea243c7570776791d975532006715fa14
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-01 12:17:54 +0530

    feat: created `ScreenContext`

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,2 +1,3 @@
 export { default as AppContext } from "./AppContext";
 export { default as ControllersContext } from "./ControllersContext";
+export { default as ScreenContext } from "./ScreenContext";
