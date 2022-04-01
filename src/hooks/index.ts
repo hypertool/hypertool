975659f10417d58d20968c44d29a3cbf53790cb8
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-01 12:18:25 +0530

    feat: created `useSymbolReference` hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useFetchApp } from "./useFetchApp";
 export { default as useApp } from "./useApp";
 export { default as useControllers } from "./useControllers";
+export { default as useSymbolReference } from "./useSymbolReference";
---
+++ src/hooks/useSymbolReference.tsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { ControllersContext } from "../contexts";
+import { ISymbolReference } from "../types";
+
+const useSymbolReference = <R,>(
+  reference?: ISymbolReference | null
+): R | null => {
+  const context = useContext(ControllersContext);
+  if (!context) {
+    throw new Error("Could not find controllers context.");
+  }
+
+  if (!reference) {
+    return null;
+  }
+
+  const controller = context[reference.moduleId];
+  if (!controller) {
+    throw new Error(
+      `Could not find controller with ID "${reference.moduleId}".`
+    );
+  }
+
+  const symbol = (controller as any)[reference.target];
+  if (!symbol) {
+    throw new Error(
+      `Could not find symbol with name "${reference.target}", in controller "${reference.moduleId}".`
+    );
+  }
+
+  return symbol as R;
+};
+
+export default useSymbolReference;
