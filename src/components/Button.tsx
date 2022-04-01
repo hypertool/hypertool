21a1f746e9c7fab5d09d85b0c8f3041960d477ec
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-01 12:20:09 +0530

    feat: updated `Button` to use `useCallbackSymbol` hook

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,32 +1,17 @@
-import {
-  FunctionComponent,
-  ReactElement,
-  useCallback,
-  useContext,
-} from "react";
-import { ControllersContext } from "../contexts";
+import { FunctionComponent, ReactElement } from "react";
+import useCallbackSymbol from "../hooks/useCallbackSymbol";
+import type { ISymbolReference } from "../types";
 
 export interface IProps {
   text?: string;
-  onClick?: {
-    moduleId: string;
-    target: string;
-  };
+  onClick?: ISymbolReference;
 }
+
+type TOnClick = () => void;
 
 const Button: FunctionComponent<IProps> = (props: IProps): ReactElement => {
   const { text, onClick } = props;
-  const { controller, context } = useContext(ControllersContext);
-  console.log(props);
-
-  const handleClick = useCallback(() => {
-    const { target = "" } = onClick || {};
-    const callback = controller[target];
-
-    if (callback) {
-      callback(context);
-    }
-  }, [context, controller, onClick]);
+  const handleClick = useCallbackSymbol<TOnClick>(onClick);
 
   return <button onClick={handleClick}>{text}</button>;
 };
