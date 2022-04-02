a351b28c244e09840c0a194fdce89ecd4566533b
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-02 10:30:42 +0530

    feat: added more properties to view component

--- packages/console/src/nodes/View.tsx
+++ packages/console/src/nodes/View.tsx
@@ -5,15 +5,79 @@ export interface IProps {
     backgroundColor?: string;
     width?: string;
     height?: string;
+    minWidth?: string;
+    minHeight?: string;
+    maxWidth?: string;
+    maxHeight?: string;
+    marginTop?: string;
+    marginRight?: string;
+    marginBottom?: string;
+    marginLeft?: string;
+    paddingTop?: string;
+    paddingRight?: string;
+    paddingBottom?: string;
+    paddingLeft?: string;
+    flexDirection?: string;
+    flexWrap?: string;
+    flexMainAxisAlignment?: string;
+    flexCrossAxisAlignment?: string;
+    flexGap?: string;
+    flexColumnGap?: string;
+    flexRowGap?: string;
     children?: ReactElement;
 }
 
 const View: FunctionComponent<IProps> = (props: IProps): ReactElement => {
-    const { backgroundColor, width, height, children } = props;
+    const {
+        backgroundColor,
+        width,
+        height,
+        minWidth,
+        minHeight,
+        maxWidth,
+        maxHeight,
+        marginTop,
+        marginRight,
+        marginBottom,
+        marginLeft,
+        paddingTop,
+        paddingRight,
+        paddingBottom,
+        paddingLeft,
+        flexDirection,
+        flexWrap,
+        flexMainAxisAlignment,
+        flexCrossAxisAlignment,
+        flexGap,
+        flexColumnGap,
+        children,
+    } = props as any;
     return (
         <div
-            onClick={() => alert("Hello, world!")}
-            style={{ width, height, backgroundColor }}
+            style={{
+                backgroundColor,
+                width,
+                height,
+                minWidth,
+                minHeight,
+                maxWidth,
+                maxHeight,
+                marginTop,
+                marginRight,
+                marginBottom,
+                marginLeft,
+                paddingTop,
+                paddingRight,
+                paddingBottom,
+                paddingLeft,
+                display: "flex",
+                flexDirection,
+                flexWrap,
+                justifyContent: flexMainAxisAlignment ?? undefined,
+                alignItems: flexCrossAxisAlignment ?? undefined,
+                gap: flexGap ?? undefined,
+                columnGap: flexColumnGap ?? undefined,
+            }}
         >
             {children}
         </div>
