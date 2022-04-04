880fe2327c80315a6adf39a33d17a3a3a15ceeca
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-04 09:45:35 +0530

    feat: updated `useControllers` to catch evaluation errors

--- src/hooks/useControllers.tsx
+++ src/hooks/useControllers.tsx
@@ -2,16 +2,25 @@ import { useMemo } from "react";
 import type { IApp, IHyperController } from "../types";
 
 /* TODO: Evaluate all the controllers, not just the ones attached to screens. */
-const useControllers = (app: IApp | null): Record<string, IHyperController<any>> =>
+const useControllers = (
+  app: IApp | null
+): Record<string, IHyperController<any>> =>
   useMemo(() => {
     const result: Record<string, IHyperController<any>> = {};
 
     if (app) {
       for (const screen of app.screens) {
         const { controller } = screen;
-        // eslint-disable-next-line no-eval
-        const object = eval(`"use strict"; (${controller.patched});`);
-        result[controller.name] = object;
+        try {
+          // eslint-disable-next-line no-eval
+          const object = eval(`"use strict"; (${controller.patched});`);
+          result[controller.name] = object;
+        } catch (error) {
+          console.log(
+            `There was an error evaluating "${controller.name}".`
+          );
+          console.log(error);
+        }
       }
     }
 
