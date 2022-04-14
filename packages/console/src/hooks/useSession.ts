1f0102350c366c39603b021cc4348cb5bac5fd7a
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-14 11:56:01 +0530

    feat: updated `useSession` to check private sessions

--- packages/console/src/hooks/useSession.ts
+++ packages/console/src/hooks/useSession.ts
@@ -3,10 +3,13 @@ import { useContext } from "react";
 import { SessionContext } from "../contexts";
 import type { ISessionContext } from "../types";
 
-const useSession = (): ISessionContext => {
+const useSession = (requirePublic = false): ISessionContext => {
     const context = useContext(SessionContext);
     if (!context) {
         throw new Error("Cannot find session context.");
+    }
+    if (!requirePublic && !context.jwtToken) {
+        throw new Error("Cannot use public session context.");
     }
     return context;
 };
