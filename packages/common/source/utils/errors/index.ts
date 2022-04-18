a8cb9540e7d851cfbb3109912f3605bc5ace784c
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-18 11:01:02 +0530

    feat: created `InternalServerError` error class

--- packages/common/source/utils/errors/index.ts
+++ packages/common/source/utils/errors/index.ts
@@ -1,3 +1,4 @@
 export { default as BadRequestError } from "./BadRequestError";
 export { default as NotFoundError } from "./NotFoundError";
 export { default as UnauthorizedError } from "./UnauthorizedError";
+export { default as InternalServerError } from "./InternalServerError";
