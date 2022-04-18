d14bd88f067a7207467bcd66afdf60b400e0cccc
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-18 11:51:21 +0530

    feat: created constants module

--- packages/api/source/utils/index.ts
+++ packages/api/source/utils/index.ts
@@ -2,4 +2,5 @@ export * from "./email";
 export * from "./auth";
 export * from "./templates";
 export * from "./query-builder";
+export * from "./constants";
 export * from "./query-execution-error";
