41fa3b38f6ef13feaa4e3d333cb11273bafc1c5f
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-18 11:51:55 +0530

    feat: updated query engine controllers

--- packages/api/source/controllers/query-engine-controllers.ts
+++ packages/api/source/controllers/query-engine-controllers.ts
@@ -1,9 +1,21 @@
-import { BadRequestError, constants } from "@hypertool/common";
+import {
+    BadRequestError,
+    InternalServerError,
+    constants,
+} from "@hypertool/common";
+import { QueryTemplateModel } from "@hypertool/common";
+import type { IResource, Query } from "@hypertool/common";
 
-import axios from "axios";
 import joi from "joi";
+import { Knex } from "knex";
 
-const { QUERY_ENGINE_URL } = process.env;
+import type { IExecuteParameters, IQueryResult } from "../types";
+import {
+    QueryBuilder,
+    QueryExecutionError,
+    errorCodes,
+    getConnection,
+} from "../utils";
 
 const executeSchema = joi.object({
     name: joi.string().max(128).required(),
@@ -14,24 +26,71 @@ const executeSchema = joi.object({
         .required(),
 });
 
-const getQueryResult = async (
-    context,
-    attributes,
-): Promise<{ result: any; error: any; meta: any }> => {
-    const { error: requestError, value } = executeSchema.validate(attributes, {
+const executeSQL = async (
+    parameters: IExecuteParameters,
+    query: Query,
+    resource: IResource,
+): Promise<any> => {
+    const sql = resource[resource.type];
+    const config: Knex.Config = {
+        client: resource.type,
+        connection: {
+            host: sql.host,
+            port: sql.port,
+            user: sql.databaseUserName,
+            password: sql.databasePassword,
+            database: sql.databaseName,
+        },
+    };
+
+    try {
+        if (typeof query.content === "object") {
+            const builder = new QueryBuilder(config);
+            const run = builder.parse(query.content);
+            return (await run())[0];
+        } else {
+            const connection = getConnection(config);
+            return (
+                await connection.raw(query.content, parameters.variables)
+            )[0];
+        }
+    } catch (error: any) {
+        throw new QueryExecutionError(
+            error.message || "An unknown error occurred while executing query.",
+            errorCodes[error.code] || "UNKNOWN",
+        );
+    }
+};
+
+const execute = async (context, attributes): Promise<IQueryResult> => {
+    const { error, value } = executeSchema.validate(attributes, {
         stripUnknown: true,
     });
-
-    if (requestError) {
-        throw new BadRequestError(requestError.message);
+    if (error) {
+        throw new BadRequestError(error.message);
     }
 
-    const result = await axios.get(QUERY_ENGINE_URL, value);
-    return result.data;
+    const query = await QueryTemplateModel.findOne({
+        name: value.name,
+    })
+        .populate("resource")
+        .exec();
+    const resource = query.resource as IResource;
+    switch (resource.type) {
+        case "mysql":
+        case "postgres": {
+            return { result: await executeSQL(value, query, resource) };
+        }
+        default: {
+            throw new InternalServerError(
+                `Unknown resource type "${resource.type}".`,
+            );
+        }
+    }
 };
 
 const getMetaData = async (context, appId: string): Promise<{ meta: any }> => {
     return null;
 };
 
-export { getQueryResult, getMetaData };
+export { execute, getMetaData };
