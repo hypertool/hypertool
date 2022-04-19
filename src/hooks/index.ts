9b73321ea7c4a79081c68f20757419cd4610bad0
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-19 11:48:20 +0530

    feat: created `useExecuteQuery` hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,4 @@ export { default as useApp } from "./useApp";
 export { default as useControllers } from "./useControllers";
 export { default as useSymbolReference } from "./useSymbolReference";
 export { default as useCallbackSymbol } from "./useCallbackSymbol";
+export { default as useExecuteQuery } from "./useExecuteQuery";
