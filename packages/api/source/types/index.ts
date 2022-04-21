348aa18d610a5507ec3da21036015313dc082662
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-21 16:51:54 +0530

    feat: created `IAuthOperation`, `IAuthResource`, and `IAuthResourceGroup` interfaces

--- packages/api/source/types/index.ts
+++ packages/api/source/types/index.ts
@@ -1,4 +1,5 @@
 import { constants } from "@hypertool/common";
+import type { IUser } from "@hypertool/common";
 
 export interface IExecuteParameters {
     name: string;
@@ -10,4 +11,19 @@ export interface IQueryResult {
     result: any;
     error?: any;
     meta?: any;
+}
+
+export interface IAuthOperation {
+    name: string;
+    test: (user: IUser, context: any) => Promise<boolean>;
+}
+
+export interface IAuthResource {
+    name: string;
+    operations: IAuthOperation[];
+}
+
+export interface IAuthResourceGroup {
+    name: string;
+    resources: IAuthResource[];
 }
