d75288fb924e9de29e7b3544096afb8232b295eb
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-21 13:20:20 +0530

    feat: updated `Controllers` component to use `Controller`

--- packages/console/src/screens/app-builder/panels/explorer/Controllers.tsx
+++ packages/console/src/screens/app-builder/panels/explorer/Controllers.tsx
@@ -1,25 +1,14 @@
-import { FunctionComponent, MouseEvent, ReactElement, useState } from "react";
-import { useCallback, useContext } from "react";
+import type { FunctionComponent, ReactElement } from "react";
+import { useCallback, useContext, useState } from "react";
 
-import {
-    Avatar,
-    Button,
-    Icon,
-    IconButton,
-    List,
-    ListItem,
-    ListItemAvatar,
-    ListItemText,
-    Menu,
-    MenuItem,
-} from "@mui/material";
+import { Button, Icon, List } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 import { gql, useMutation, useQuery } from "@apollo/client";
 
-import { useConfirm } from "material-ui-confirm";
-
 import { BuilderActionsContext } from "../../../../contexts";
+
+import Controller from "./Controller";
 
 const Actions = styled("div")(({ theme }) => ({
     display: "flex",
@@ -28,8 +17,6 @@ const Actions = styled("div")(({ theme }) => ({
         2,
     )} ${theme.spacing(2)}`,
 }));
-
-const StyledListItemAvatar = styled(ListItemAvatar)({ minWidth: 32 });
 
 const GET_CONTROLLERS = gql`
     query GetControllers($page: Int, $limit: Int) {
@@ -54,8 +41,6 @@ const DELETE_CONTROLLER = gql`
 
 const Controllers: FunctionComponent = (): ReactElement => {
     const { createTab } = useContext(BuilderActionsContext);
-    const [anchor, setAnchor] = useState<HTMLElement | null>(null);
-    const confirm = useConfirm();
 
     const { data } = useQuery(GET_CONTROLLERS, {
         variables: {
@@ -73,90 +58,32 @@ const Controllers: FunctionComponent = (): ReactElement => {
         createTab("new-controller");
     }, [createTab]);
 
-    const handleEditController =
-        (controllerId: string) => (event: MouseEvent<HTMLElement>) => {
-            createTab("edit-controller", { controllerId });
-            event.stopPropagation();
-            setAnchor(null);
-        };
-
-    const handleOpenMenu =
-        (controllerId: string) => (event: MouseEvent<HTMLElement>) => {
-            event.stopPropagation();
-            setAnchor(event.currentTarget);
-        };
-
-    const handleCloseMenu = useCallback(() => {
-        setAnchor(null);
+    const handleEditController = useCallback((controllerId: string) => {
+        createTab("edit-controller", { controllerId });
     }, []);
 
-    const handleDeleteController =
-        (controllerId: string) => async (event: MouseEvent<HTMLElement>) => {
-            event.stopPropagation();
-            setAnchor(null);
-
-            try {
-                await confirm({
-                    title: "Are you sure you want to delete?",
-                    description:
-                        "This action cannot be undone. This will permanently delete the controller.",
-                    confirmationText: "Delete",
-                    cancellationText: "Cancel",
-                    allowClose: true,
-                });
-                deleteController({
-                    variables: {
-                        controllerId,
-                    },
-                });
-            } catch (error: unknown) {}
-        };
-
-    const renderController = (record: any) => (
-        <ListItem
-            key={record.id}
-            button={true}
-            secondaryAction={
-                <IconButton edge="end" onClick={handleOpenMenu(record.id)}>
-                    <Icon fontSize="small">more_vert</Icon>
-                </IconButton>
-            }
-            onClick={handleEditController(record.id)}
-        >
-            <StyledListItemAvatar>
-                <Avatar style={{ width: 20, height: 20 }}>
-                    <Icon style={{ fontSize: 14 }}>code</Icon>
-                </Avatar>
-            </StyledListItemAvatar>
-            <ListItemText primary={record.name} />
-            <Menu
-                anchorEl={anchor}
-                keepMounted={true}
-                anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                }}
-                transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                }}
-                open={Boolean(anchor)}
-                onClose={handleCloseMenu}
-            >
-                <MenuItem onClick={handleEditController(record.id)}>
-                    Edit
-                </MenuItem>
-                <MenuItem onClick={handleDeleteController(record.id)}>
-                    Delete
-                </MenuItem>
-            </Menu>
-        </ListItem>
-    );
+    const handleDeleteController = useCallback((controllerId: string) => {
+        deleteController({
+            variables: {
+                controllerId,
+            },
+        });
+    }, []);
 
     return (
         <>
             <div>
-                <List dense={true}>{records.map(renderController)}</List>
+                <List dense={true}>
+                    {records.map((controller: any) => (
+                        <Controller
+                            key={controller.id}
+                            id={controller.id}
+                            name={controller.name}
+                            onEdit={handleEditController}
+                            onDelete={handleDeleteController}
+                        />
+                    ))}
+                </List>
                 <Actions>
                     <Button
                         size="small"
