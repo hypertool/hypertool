63f50ed68e0d2d9230a3f3f3957c683035b988ba
Samuel Rowe <samuelrowe1999@gmail.com>
2022-04-30 09:55:35 +0530

    feat: updated new app screen

--- packages/console/src/screens/new-app/index.tsx
+++ packages/console/src/screens/new-app/index.tsx
@@ -1,28 +1,165 @@
-import { FunctionComponent, ReactElement } from "react";
+import type { FunctionComponent, ReactElement } from "react";
+import { useCallback, useMemo, useState } from "react";
 
-import {
-    Container as MuiContainer,
-    useMediaQuery,
-    useTheme,
-} from "@mui/material";
+import { Container as MuiContainer } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-import { Wrap } from "../../components";
+import { gql, useMutation } from "@apollo/client";
 
-import NewAppScreen from "./NewAppScreen";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
 
-const Container = styled(MuiContainer)(({ theme }) => ({
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(2),
-}));
+import Stepper, { IStep } from "../../components/Stepper";
+import { useNotification, usePrivateSession } from "../../hooks";
+
+import AboutStep from "./AboutStep";
+import SelectTemplateStep from "./SelectTemplateStep";
+
+const Container = styled(MuiContainer)(({ theme }) => ({}));
+
+const CREATE_APP = gql`
+    mutation CreateApp(
+        $name: String!
+        $title: String!
+        $description: String!
+        $organization: ID
+    ) {
+        createApp(
+            name: $name
+            title: $title
+            organization: $organization
+            description: $description
+        ) {
+            id
+        }
+    }
+`;
+
+interface IFormValues {
+    name: string;
+    title: string;
+    description: string;
+}
+
+const initialValues: IFormValues = {
+    name: "",
+    title: "",
+    description: "",
+};
+
+const validationSchema = yup.object({
+    name: yup
+        .string()
+        .max(128, "Name should be 128 characters or less")
+        .required("Name is required"),
+    title: yup
+        .string()
+        .max(256, "Title should be 128 characters or less")
+        .required("Title is required"),
+    description: yup
+        .string()
+        .max(512, "Description should be 512 characters or less"),
+});
 
 const NewApp: FunctionComponent = (): ReactElement => {
-    const theme = useTheme();
-    const matches = useMediaQuery(theme.breakpoints.up("lg"));
+    const navigate = useNavigate();
+    const notification = useNotification();
+    const [createApp, { loading, data }] = useMutation(CREATE_APP, {
+        refetchQueries: ["GetApps"],
+    });
+    const [template, setTemplate] = useState<"$empty" | string>("$empty");
+
+    const handleTemplateChange = useCallback((id: string) => {
+        setTemplate(id);
+    }, []);
+
+    const steps: IStep[] = useMemo(
+        () => [
+            {
+                title: "About app",
+                optional: false,
+                renderStep: () => <AboutStep />,
+            },
+            {
+                title: "Choose template",
+                optional: true,
+                renderStep: () => (
+                    <SelectTemplateStep
+                        activeTemplate={template}
+                        onChange={handleTemplateChange}
+                    />
+                ),
+            },
+        ],
+        [template],
+    );
+
+    const handleComplete = useCallback(
+        async (values: IFormValues): Promise<void> => {
+            try {
+                notification.notify({
+                    type: "info",
+                    message: `Creating screen "${values.name}"...`,
+                    closeable: false,
+                    autoCloseDuration: -1,
+                });
+
+                const result = await createApp({
+                    variables: {
+                        ...values,
+                        sourceApp: template === "$empty" ? undefined : template,
+                    },
+                });
+
+                notification.notifySuccess(
+                    `Created screen "${values.name}" successfully`,
+                );
+
+                navigate(`/apps/${result.data.createApp.id}/builder`);
+            } catch (error) {
+                notification.notifyError(error);
+            }
+        },
+        [],
+    );
+
+    const isStepComplete = useCallback((step: number, context: any) => {
+        const { values, errors } = context;
+        switch (step) {
+            case 0: {
+                return (
+                    values.name &&
+                    values.title &&
+                    !errors.name &&
+                    !errors.title &&
+                    !errors.description
+                );
+            }
+
+            case 1: {
+                return true;
+            }
+
+            default: {
+                throw new Error(
+                    `The specified step number (${step}) does not exist!`,
+                );
+            }
+        }
+    }, []);
+
     return (
-        <Wrap when={matches} wrapper={Container}>
-            <NewAppScreen />
-        </Wrap>
+        <Container>
+            <Stepper
+                title="Create new app"
+                description="An app provides an interface for the user to interact with your platform on Android, iOS, and web."
+                steps={steps}
+                onComplete={handleComplete}
+                isStepComplete={isStepComplete}
+                validationSchema={validationSchema}
+                initialValues={initialValues}
+            />
+        </Container>
     );
 };
 
