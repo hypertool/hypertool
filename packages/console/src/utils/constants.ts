63e688abe8cfd56694530b41cd58c38e3a3d791f
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-16 22:37:22 +0530

    feat: created a few constants
     * Created the `tuple` utility function for creating constants

--- packages/console/src/utils/constants.ts
+++ packages/console/src/utils/constants.ts
@@ -1,4 +1,6 @@
-export const tabTypes = [
+const tuple = <T extends string[]>(...values: T) => values;
+
+export const tabTypes = tuple(
     "edit-query",
     "new-query",
     "new-controller",
@@ -11,8 +13,64 @@ export const tabTypes = [
     "authentication.new-user",
     "authentication.view-providers",
     "authentication.new-provider",
-];
+);
 
-export const controllerLanguages = ["javascript", "typescript"];
+export const controllerLanguages = tuple("javascript", "typescript");
 
 export const slugPattern = /^(\/:?[-_.a-zA-Z0-9]+)+$/;
+
+export const providerTypes = tuple("email_password", "anonymous");
+
+export const resourceStatuses = tuple(
+    "enabled",
+    "disabled",
+    "deleted",
+    "banned",
+);
+
+export const resourceTypes = tuple(
+    // Databases
+    "mysql",
+    "postgres",
+    "microsoft_sql",
+    "mongodb",
+    "cassandra",
+    "cosmosdb",
+    "amazon_redshift",
+    "amazon_athena",
+    "bigquery",
+    "elasticsearch",
+    "couchdb",
+    "rethinkdb",
+    "snowflake",
+    "denodo",
+    "redis",
+    "dynamodb",
+
+    // APIs
+    "rest_api",
+    "graphql",
+    "firebase",
+    "stripe",
+    "twilio",
+    "github",
+    "google_sheets",
+    "salesforce",
+    "sendgrid",
+    "amazon_s3",
+    "google_cloud_storage",
+    "datadog",
+    "lambda",
+    "openapi",
+    "smtp",
+    "slack",
+    "asana",
+    "jira",
+    "close",
+    "bigid",
+    "basecamp",
+    "onesignal",
+    "front",
+    "google_maps",
+    "circleci",
+);
