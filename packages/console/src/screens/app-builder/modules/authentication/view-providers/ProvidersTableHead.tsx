3c3821c7c85ea969dd1897d291b88268fd6296b7
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-16 23:38:30 +0530

    feat: created `ProvidersTableHead` component

---
+++ packages/console/src/screens/app-builder/modules/authentication/view-providers/ProvidersTableHead.tsx
@@ -0,0 +1,64 @@
+import type { ChangeEvent, FunctionComponent, ReactElement } from "react";
+
+import { Checkbox, TableCell, TableHead, TableRow } from "@mui/material";
+
+interface HeadCell {
+    id: string;
+    numeric: boolean;
+    disablePadding: boolean;
+    label: string;
+}
+
+const headCells: readonly HeadCell[] = [
+    {
+        id: "provider",
+        numeric: false,
+        disablePadding: true,
+        label: "Provider",
+    },
+    {
+        id: "status",
+        numeric: false,
+        disablePadding: false,
+        label: "Status",
+    },
+];
+
+export interface IProvidersTableHeadProps {
+    selectedCount: number;
+    onSelectAllClick: (event: ChangeEvent<HTMLInputElement>) => void;
+    rowCount: number;
+}
+
+const ProvidersTableHead: FunctionComponent<IProvidersTableHeadProps> = (
+    props: IProvidersTableHeadProps,
+): ReactElement => {
+    const { onSelectAllClick, selectedCount, rowCount } = props;
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        color="primary"
+                        indeterminate={
+                            selectedCount > 0 && selectedCount < rowCount
+                        }
+                        checked={rowCount > 0 && selectedCount === rowCount}
+                        onChange={onSelectAllClick}
+                    />
+                </TableCell>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? "right" : "left"}
+                        padding={headCell.disablePadding ? "none" : "normal"}
+                    >
+                        {headCell.label}
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+};
+
+export default ProvidersTableHead;
