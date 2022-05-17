00cf82b08701ca4505997a23a0f4c4b8387ebb7a
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-17 11:15:24 +0530

    feat: created dummy `NewProviderEditor` component

---
+++ packages/console/src/screens/app-builder/modules/authentication/new-provider-editor/index.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent, ReactElement } from "react";
+
+const NewProviderEditor: FunctionComponent = (): ReactElement => {
+    return <div>New Provider Editor</div>;
+};
+
+export default NewProviderEditor;
