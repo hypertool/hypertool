5bf845e4b444c5f144712a7520d8778df491c751
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-18 19:54:07 +0530

    feat: created `sendVerificationEmail` utility function

--- packages/api/source/utils/email.ts
+++ packages/api/source/utils/email.ts
@@ -1,5 +1,8 @@
 import sgMail from "@sendgrid/mail";
 
+import { createToken } from "./auth";
+import { renderTemplate } from "./templates";
+
 export const sendEmail = async (params: any): Promise<void> => {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const { to, subject, text, html, from } = params;
@@ -11,4 +14,19 @@ export const sendEmail = async (params: any): Promise<void> => {
         html,
     };
     await sgMail.send(message);
+};
+
+export const sendVerificationEmail = async (emailAddress: string) => {
+    const token = createToken({ emailAddress }, "30m");
+    const verificationURL = `http://localhost:3001/api/v1/users/verify/${token}`;
+    const params = {
+        from: { name: "Hypertool", email: "noreply@hypertool.io" },
+        to: emailAddress,
+        subject: "Verify your Hypertool email address",
+        text: `Open the following link to validate your email address: ${verificationURL}`,
+        html: await renderTemplate("verify-email.html", {
+            verificationURL,
+        }),
+    };
+    await sendEmail(params);
 };
