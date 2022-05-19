b2a3c92075f32f1ce1f07757dd6cbf02a2de7dc4
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-19 09:44:26 +0530

    feat: created dummy `UserEditor` component

--- packages/console/src/screens/app-builder/modules/authentication/index.ts
+++ packages/console/src/screens/app-builder/modules/authentication/index.ts
@@ -2,3 +2,4 @@ export { default as ViewProviders } from "./view-providers";
 export { default as NewProviderEditor } from "./new-provider-editor";
 export { default as ViewUsers } from "./view-users";
 export { default as NewUserEditor } from "./new-user-editor";
+export { default as UserEditor } from "./user-editor";
---
+++ packages/console/src/screens/app-builder/modules/authentication/user-editor/index.tsx
@@ -0,0 +1,7 @@
+import type { FunctionComponent, ReactElement } from "react";
+
+const UserEditor: FunctionComponent = (): ReactElement => {
+    return <div>User Editor</div>;
+};
+
+export default UserEditor;
