aa2573be2852406fe8de9979e047259066effcfa
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-23 10:27:32 +0530

    feat: created stepper for installation screen

--- packages/console/src/screens/installation/index.tsx
+++ packages/console/src/screens/installation/index.tsx
@@ -1,7 +1,19 @@
 import type { FunctionComponent, ReactElement } from "react";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
+
+import { Container as MuiContainer, styled } from "@mui/material";
+
+import * as yup from "yup";
+
+import Stepper, { IStep } from "../../components/Stepper";
 
 import Welcome from "./Welcome";
+
+const Container = styled(MuiContainer)(({ theme }) => ({}));
+
+const validationSchema = yup.object();
+
+const initialValues = {};
 
 const Installation: FunctionComponent = (): ReactElement => {
     const [start, setStart] = useState(false);
@@ -10,10 +22,53 @@ const Installation: FunctionComponent = (): ReactElement => {
         setStart(true);
     }, []);
 
+    const steps: IStep[] = useMemo(
+        () => [
+            {
+                title: "Root app details",
+                optional: false,
+                renderStep: () => <div>App details</div>,
+            },
+            {
+                title: "Administrator details",
+                optional: false,
+                renderStep: () => <div>User details</div>,
+            },
+            {
+                title: "Hypertool Newsletter",
+                optional: false,
+                renderStep: () => <div>User details</div>,
+            },
+        ],
+        [],
+    );
+
+    const isStepComplete = useCallback(
+        (step: number, context: any) => false,
+        [],
+    );
+
+    const handleComplete = useCallback(async (values: any): Promise<void> => {
+        return;
+    }, []);
+
     return (
         <div>
             {!start && <Welcome onPrimaryAction={handleStart} />}
-            {start && <div>Stepper</div>}
+            {start && (
+                <Container>
+                    <Stepper
+                        title="Install Hypertool"
+                        description="Hypertool is an open-source low code platform that helps you build internal tools quickly."
+                        steps={steps}
+                        onComplete={handleComplete}
+                        isStepComplete={isStepComplete}
+                        validationSchema={validationSchema}
+                        initialValues={initialValues}
+                        finalActionText="Create Provider"
+                    />
+                </Container>
+            )}
         </div>
     );
 };
