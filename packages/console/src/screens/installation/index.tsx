daa27dea40c5e100efeb4104cfb4ba8ec1dd9bdf
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-24 11:47:46 +0530

    feat: updated `Install` component to implement the steps

--- packages/console/src/screens/installation/index.tsx
+++ packages/console/src/screens/installation/index.tsx
@@ -7,13 +7,57 @@ import * as yup from "yup";
 
 import Stepper, { IStep } from "../../components/Stepper";
 
+import AdministratorDetailsStep from "./AdministratorDetailsStep";
+import AppDetailsStep from "./AppDetailsStep";
 import Welcome from "./Welcome";
 
 const Container = styled(MuiContainer)(({ theme }) => ({}));
 
-const validationSchema = yup.object();
+interface IFormValues {
+    name: string;
+    title: string;
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+    password: string;
+}
 
-const initialValues = {};
+const validationSchema = yup.object({
+    name: yup
+        .string()
+        .max(128, "Name should be 128 characters or less")
+        .required("Name is required"),
+    title: yup
+        .string()
+        .max(256, "Title should be 128 characters or less")
+        .required("Title is required"),
+    firstName: yup
+        .string()
+        .max(30, "First name should be 30 characters or less")
+        .required("First name is required"),
+    lastName: yup
+        .string()
+        .max(30, "Last name should be 30 characters or less")
+        .required("Last name is required"),
+    emailAddress: yup
+        .string()
+        .max(255, "Email address should be 255 characters or less")
+        .required("Email address is required"),
+    password: yup
+        .string()
+        .min(8, "Password should be 8 characters or more")
+        .max(128, "Password should be 128 characters or less")
+        .required("Password is required"),
+});
+
+const initialValues: IFormValues = {
+    name: "",
+    title: "",
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    password: "",
+};
 
 const Installation: FunctionComponent = (): ReactElement => {
     const [start, setStart] = useState(false);
@@ -27,26 +71,53 @@ const Installation: FunctionComponent = (): ReactElement => {
             {
                 title: "Root app details",
                 optional: false,
-                renderStep: () => <div>App details</div>,
+                renderStep: () => <AppDetailsStep />,
             },
             {
                 title: "Administrator details",
                 optional: false,
-                renderStep: () => <div>User details</div>,
+                renderStep: () => <AdministratorDetailsStep />,
             },
-            {
-                title: "Hypertool Newsletter",
-                optional: false,
-                renderStep: () => <div>User details</div>,
-            },
+            /*
+             * {
+             *     title: "Hypertool Newsletter",
+             *     optional: false,
+             *     renderStep: () => <NewsletterStep />,
+             * },
+             */
         ],
         [],
     );
 
-    const isStepComplete = useCallback(
-        (step: number, context: any) => false,
-        [],
-    );
+    const isStepComplete = useCallback((step: number, context: any) => {
+        const { values, errors } = context;
+        switch (step) {
+            case 0: {
+                return (
+                    values.name && values.title && !errors.name && !errors.title
+                );
+            }
+
+            case 1: {
+                return (
+                    values.firstName &&
+                    values.lastName &&
+                    values.emailAddress &&
+                    values.password &&
+                    !errors.firstName &&
+                    !errors.lastName &&
+                    !errors.emailAddress &&
+                    !errors.password
+                );
+            }
+
+            default: {
+                throw new Error(
+                    `The specified step number (${step}) does not exist!`,
+                );
+            }
+        }
+    }, []);
 
     const handleComplete = useCallback(async (values: any): Promise<void> => {
         return;
@@ -65,7 +136,7 @@ const Installation: FunctionComponent = (): ReactElement => {
                         isStepComplete={isStepComplete}
                         validationSchema={validationSchema}
                         initialValues={initialValues}
-                        finalActionText="Create Provider"
+                        finalActionText="Install Hypertool"
                     />
                 </Container>
             )}
