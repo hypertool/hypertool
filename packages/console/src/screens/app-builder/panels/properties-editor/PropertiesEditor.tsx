b05b22f92f31a3d6b0075ca0f3d96ea3034ad551
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-25 22:38:44 +0530

    feat: added save button to properties editor

--- packages/console/src/screens/app-builder/panels/properties-editor/PropertiesEditor.tsx
+++ packages/console/src/screens/app-builder/panels/properties-editor/PropertiesEditor.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import { createElement, useCallback } from "react";
 import type { FunctionComponent, ReactElement } from "react";
 
+import { Button, styled } from "@mui/material";
+
+import { gql, useMutation } from "@apollo/client";
+
 import { useEditor } from "../../../../craft";
+import { useNotification, useTabBundle } from "../../../../hooks";
+import { IEditScreenBundle } from "../../../../types";
+
+const Root = styled("div")(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+}));
+
+const Properties = styled("div")(({ theme }) => ({
+    minHeight: "calc(100vh - 148px)",
+}));
+
+const Actions = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+}));
+
+const UPDATE_SCREEN = gql`
+    mutation UpdateScreen($screenId: ID!, $content: String!) {
+        updateScreen(screenId: $screenId, content: $content) {
+            id
+        }
+    }
+`;
 
 const PropertiesEditor: FunctionComponent = (): ReactElement => {
-    const { selected } = useEditor((state) => {
+    const { selected, query } = useEditor((state) => {
         const currentNodeId: Set<string> = state.events.selected;
         if (currentNodeId) {
             const id = currentNodeId.values().next().value;
@@ -25,11 +52,43 @@ const PropertiesEditor: FunctionComponent = (): ReactElement => {
         };
     }) as any;
 
+    const bundle = useTabBundle<IEditScreenBundle>();
+    const notification = useNotification();
+    const [updateScreen] = useMutation(UPDATE_SCREEN, {
+        refetchQueries: ["GetScreen"],
+    });
+
+    const handleSave = useCallback(async () => {
+        try {
+            const content = query.serialize();
+            await updateScreen({
+                variables: {
+                    screenId: bundle.screenId,
+                    content,
+                },
+            });
+        } catch (error: any) {
+            notification.notifyError(error);
+        }
+    }, [query, updateScreen, bundle, notification]);
+
     return (
-        <>
-            {selected?.settings &&
-                React.createElement((selected as any).settings)}
-        </>
+        <Root>
+            <Properties>
+                {selected?.settings &&
+                    createElement((selected as any).settings)}
+            </Properties>
+            <Actions>
+                <Button
+                    variant="contained"
+                    fullWidth={true}
+                    type="submit"
+                    onClick={handleSave}
+                >
+                    Save
+                </Button>
+            </Actions>
+        </Root>
     );
 };
 
