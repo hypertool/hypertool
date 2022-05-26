b88a1971507762a96744150afa98ea8f967aa60b
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-27 00:29:49 +0530

    feat: added width and height properties to table cell component

--- packages/console/src/nodes/TableCell.tsx
+++ packages/console/src/nodes/TableCell.tsx
@@ -1,6 +1,6 @@
 import type { FunctionComponent, ReactElement, ReactNode } from "react";
 
-import { TableCell as MuiTableCell } from "@mui/material";
+import { TableCell as MuiTableCell, styled } from "@mui/material";
 
 export interface ITableCellProps {
     id?: string;
@@ -8,22 +8,49 @@ export interface ITableCellProps {
     padding?: "checkbox" | "none" | "normal" | "inherit";
     size?: "small" | "medium" | "inherit";
     variant?: "body" | "footer" | "head";
+    height?: string;
+    width?: string;
     children?: ReactNode;
 }
+
+const EmptyCellContainer = styled("div")(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+}));
+
+const EmptyCellText = styled("p")(({ theme }) => ({
+    color: "black",
+    fontStyle: "italic",
+    fontSize: 12,
+    textAlign: "center",
+    width: "100%",
+    padding: 0,
+    margin: 0,
+}));
 
 const TableCell: FunctionComponent<ITableCellProps> = (
     props: ITableCellProps,
 ): ReactElement => {
-    const { children, align, padding, size, variant } = props;
+    const { children, align, padding, size, variant, height, width } = props;
     return (
         <MuiTableCell
             align={align}
             padding={padding === "inherit" ? undefined : padding}
             size={size === "inherit" ? undefined : size}
             variant={variant}
-            style={{ display: "flex", height: "fit-content", width: 200 }}
+            style={{
+                display: "flex",
+                ...(children ? { height, width } : { height: 56, width: 200 }),
+            }}
         >
-            {children}
+            {children || (
+                <EmptyCellContainer>
+                    <EmptyCellText>Empty table cell</EmptyCellText>
+                </EmptyCellContainer>
+            )}
         </MuiTableCell>
     );
 };
@@ -33,6 +60,8 @@ TableCell.defaultProps = {
     padding: "inherit",
     size: "inherit",
     variant: "body",
+    height: "56px",
+    width: "200px",
 };
 
 export default TableCell;
