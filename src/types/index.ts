dc7765e0415e758397eb15bd3b940f01fbb8fa2c
Samuel Rowe <samuelrowe1999@gmail.com>
2022-05-30 11:23:41 +0530

    feat: created `TSymbolReferenceOrFunction` type

--- src/types/index.ts
+++ src/types/index.ts
@@ -62,6 +62,8 @@ export interface ISymbolReference {
   target: string;
 }
 
+export type TSymbolReferenceOrFunction = ISymbolReference | Function;
+
 export type TPropertyValue =
   | number
   | boolean
