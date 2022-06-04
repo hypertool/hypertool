7f8d70407c0b347e624ae019c214dff1e492739a
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-04 15:05:48 +0530

    fix: fixed incorrect conditional invocation of `useContext`

--- packages/console/src/hooks/useSymbolReference.ts
+++ packages/console/src/hooks/useSymbolReference.ts
@@ -6,11 +6,12 @@ import { ModulesContext } from "../contexts";
 import { ISymbolReference } from "../types";
 
 const useSymbolReference = (reference?: ISymbolReference): any | undefined => {
+    const modules = useContext(ModulesContext);
+
     if (!reference) {
         return undefined;
     }
 
-    const modules = useContext(ModulesContext);
     const module = modules[reference.moduleId];
     if (!module) {
         throw new Error(`Cannot find module with ID "${reference.moduleId}"`);
