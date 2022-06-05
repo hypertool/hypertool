4d442c0d19d4f56b5514c1389f841c1612cead3e
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-05 18:15:27 +0530

    fix: provided multiple fallback port numbers

--- packages/api/source/start.ts
+++ packages/api/source/start.ts
@@ -8,13 +8,13 @@ dotenv.config({
     path: `${__dirname}/../../../.env`,
 });
 
-const { API_PORT, DATABASE_URL } = process.env;
+const { API_PORT, PORT, DATABASE_URL } = process.env;
 
 mongoose.set("debug", true);
 mongoose.connection.once("open", async () => {
     console.log(" ✅ Database connection successfully established");
     const app = await initialize();
-    http.createServer(app).listen(API_PORT, () => {
+    http.createServer(app).listen(PORT || API_PORT || 3000, () => {
         console.log(
             ` 🎉 You can access the server at http://localhost:${API_PORT}`,
         );
