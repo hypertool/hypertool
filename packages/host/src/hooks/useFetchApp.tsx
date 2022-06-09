98e98004feaf00a7a5423147255b8bd137aa1694
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-09 10:30:27 +0530

    feat: updated to use public domain from env

--- packages/host/src/hooks/useFetchApp.tsx
+++ packages/host/src/hooks/useFetchApp.tsx
@@ -3,79 +3,80 @@ import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { IApp } from "../types";
 
 const client = new ApolloClient({
-  uri: `${process.env.REACT_APP_API_URL}/graphql/v1/public`,
-  cache: new InMemoryCache(),
+    uri: `${process.env.REACT_APP_API_URL}/graphql/v1/public`,
+    cache: new InMemoryCache(),
 });
 
 const GET_APP = gql`
-  query GetApp($name: String!) {
-    getAppByName(name: $name) {
-      id
-      name
-      title
-      description
-      status
-      createdAt
-      updatedAt
-      screens {
-        id
-        name
-        title
-        description
-        slug
-        content
-        controller {
-          id
-          name
-          description
-          language
-          patched
-          status
-          createdAt
-          updatedAt
+    query GetApp($name: String!) {
+        getAppByName(name: $name) {
+            id
+            name
+            title
+            description
+            status
+            createdAt
+            updatedAt
+            screens {
+                id
+                name
+                title
+                description
+                slug
+                content
+                controller {
+                    id
+                    name
+                    description
+                    language
+                    patched
+                    status
+                    createdAt
+                    updatedAt
+                }
+                status
+                createdAt
+                updatedAt
+            }
         }
-        status
-        createdAt
-        updatedAt
-      }
     }
-  }
 `;
 
-const publicDomainSuffix = ".hypertool.io";
-
 const useFetchApp = (): IApp | null => {
-  const [app, setApp] = useState<IApp | null>(null);
+    const [app, setApp] = useState<IApp | null>(null);
 
-  useEffect(() => {
-    let mounted = true;
-    (async () => {
-      const hostName = window.location.hostname;
-      if (!hostName.endsWith(publicDomainSuffix)) {
-        throw new Error("Custom domains are not supported.");
-      }
+    useEffect(() => {
+        let mounted = true;
+        (async () => {
+            const publicDomainSuffix = `.${process.env.REACT_APP_PUBLIC_DOMAIN}`;
+            const hostName = window.location.hostname;
+            if (!hostName.endsWith(publicDomainSuffix)) {
+                throw new Error(
+                    `Custom domains are currently not supported. Expected domain "${process.env.REACT_APP_PUBLIC_DOMAIN}", but found "${hostName}".`,
+                );
+            }
 
-      const appName = hostName.substring(
-        0,
-        hostName.length - publicDomainSuffix.length
-      );
-      const result = await client.query({
-        query: GET_APP,
-        variables: {
-          name: appName,
-        },
-      });
-      if (mounted) {
-        setApp(result.data.getAppByName);
-      }
-    })();
+            const appName = hostName.substring(
+                0,
+                hostName.length - publicDomainSuffix.length,
+            );
+            const result = await client.query({
+                query: GET_APP,
+                variables: {
+                    name: appName,
+                },
+            });
+            if (mounted) {
+                setApp(result.data.getAppByName);
+            }
+        })();
 
-    return () => {
-      mounted = false;
-    };
-  }, []);
+        return () => {
+            mounted = false;
+        };
+    }, []);
 
-  return app;
+    return app;
 };
 
 export default useFetchApp;
