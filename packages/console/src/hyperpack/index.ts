cefb4b17463a3ebd779c1e73528dc395a859c376
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-15 10:27:33 +0530

    feat: created `esbuild` plugin for loading files

---
+++ packages/console/src/hyperpack/index.ts
@@ -0,0 +1,27 @@
+import type { Plugin, PluginBuild } from "esbuild-wasm";
+
+const source = `
+const main = (): void => {
+    console.log("Hello, world!");
+};
+`;
+
+export const hyperpack = (): Plugin => {
+    return {
+        name: "hyperpack",
+        setup: (build: PluginBuild) => {
+            const { onResolve, onLoad } = build;
+            onResolve({ filter: /.*/ }, (values) => {
+                return {
+                    path: "/" + values.path.replace(/^\.\//, ""),
+                    external: false,
+                };
+            });
+
+            onLoad({ filter: /.*/ }, (values) => {
+                const loader = "default";
+                return { contents: source, loader };
+            });
+        },
+    };
+};
