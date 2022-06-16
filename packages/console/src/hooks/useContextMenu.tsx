066c986c87235c2b3f4188158c20fff5c79fe484
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-16 14:08:04 +0530

    feat: updated `useContextMenu` to return mouse X and Y coordinates

--- packages/console/src/hooks/useContextMenu.tsx
+++ packages/console/src/hooks/useContextMenu.tsx
@@ -3,20 +3,28 @@ import { useCallback, useState } from "react";
 
 const useContextMenu = () => {
     const [anchor, setAnchor] = useState<HTMLElement | null>(null);
+    const [mouseX, setMouseX] = useState<number>(0);
+    const [mouseY, setMouseY] = useState<number>(0);
 
     const onContextMenuOpen = useCallback((event: MouseEvent<HTMLElement>) => {
         event.preventDefault();
         event.stopPropagation();
 
         setAnchor(event.currentTarget);
+        setMouseX(event.clientX);
+        setMouseY(event.clientY);
     }, []);
 
     const onContextMenuClose = useCallback(() => {
         setAnchor(null);
+        setMouseX(0);
+        setMouseY(0);
     }, []);
 
     return {
         anchor,
+        mouseX,
+        mouseY,
         onContextMenuOpen,
         onContextMenuClose,
     };
