c0a9cb3f15af3f6227a5b2f657c2da341cf95e1e
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-23 00:24:33 +0530

    fix: fixed incorrect tree generation from path list

--- packages/console/src/utils/files.ts
+++ packages/console/src/utils/files.ts
@@ -40,33 +40,40 @@ export const createPathTree = (
 
     const level: IPathLevel = { "<result>": [] as IPathNode[] };
 
-    paths.forEach((path) => {
-        (path.name[0] === "/" ? path.name.replace("/", "") : path.name)
-            .split("/")
-            .reduce(
-                ((
-                    currentLevel: IPathLevel,
-                    name: string,
-                    index: number,
-                    array: string[],
-                ) => {
-                    if (!currentLevel[name]) {
-                        currentLevel[name] = { "<result>": [] };
-                        (currentLevel["<result>"] as IPathNode[]).push({
-                            name,
-                            children: (currentLevel[name] as IPathLevel)[
-                                "<result>"
-                            ] as IPathNode[],
-                            /* Attach the path object to the leaf node. */
-                            path: index === array.length - 1 ? path : null,
-                        });
-                    }
-
-                    return currentLevel[name];
-                }) as any,
-                level,
-            );
-    });
+    /* The paths need to sorted to correctly assign path references. AFAIK,
+     * this is a bruteforce approach and a quicker way may exist. Unfortunately,
+     * I lack both intellect and time to solve this optimization.
+     */
+    [...paths]
+        .sort((path1, path2) => path1.name.localeCompare(path2.name))
+        .forEach((path) => {
+            path.name
+                .split("/")
+                /* Get rid of all the prefix, suffix, and consecutive forward
+                 * slashes to extract just the path elements.
+                 */
+                .filter(Boolean)
+                .reduce(
+                    ((currentLevel: IPathLevel, name: string) => {
+                        if (!currentLevel[name]) {
+                            currentLevel[name] = { "<result>": [] };
+                            const ref = {
+                                name,
+                                children: (currentLevel[name] as IPathLevel)[
+                                    "<result>"
+                                ] as IPathNode[],
+                                /* Attach the path object to the leaf node. */
+                                path,
+                            };
+                            (currentLevel["<result>"] as IPathNode[]).push(
+                                ref as any,
+                            );
+                        }
+                        return currentLevel[name];
+                    }) as any,
+                    level,
+                );
+        });
 
     const finalArray = level["<result>"] as IPathNode[];
 
