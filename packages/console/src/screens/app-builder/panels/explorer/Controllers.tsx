40389fcbfc7d5ebe3191cf21fa7d989681181131
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-23 19:49:39 +0530

    feat: integrated `Controllers` component with GraphQL API

--- packages/console/src/screens/app-builder/panels/explorer/Controllers.tsx
+++ packages/console/src/screens/app-builder/panels/explorer/Controllers.tsx
@@ -1,107 +1,171 @@
-import Collapse from "@mui/material/Collapse";
-import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon";
-import { alpha, styled } from "@mui/material/styles";
-import { TransitionProps } from "@mui/material/transitions";
+import { useCallback, useMemo } from "react";
 
-import TreeItem, { TreeItemProps, treeItemClasses } from "@mui/lab/TreeItem";
-import TreeView from "@mui/lab/TreeView";
+import { TreeView } from "@mui/lab";
 
-import { files } from "../../../../utils";
+import FileTreeNode from "./FileTreeNode";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { createPathTree } from "../../../../utils/files";
+import { useParam } from "../../../../hooks";
+import { Icon } from "@mui/material";
+import { IPathNode, TNewFileType } from "../../../../types";
 
-function MinusSquare(props: SvgIconProps) {
-    return (
-        <SvgIcon
-            fontSize="inherit"
-            style={{ width: 14, height: 14 }}
-            {...props}
-        >
-            {/* tslint:disable-next-line: max-line-length */}
-            <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
-        </SvgIcon>
-    );
+const GET_SOURCE_FILES = gql`
+    query GetSourceFiles($app: ID!, $page: Int, $limit: Int) {
+        getSourceFiles(app: $app, page: $page, limit: $limit) {
+            totalPages
+            records {
+                id
+                name
+                directory
+            }
+        }
+    }
+`;
+
+const CREATE_SOURCE_FILE = gql`
+    mutation CreateSourceFile(
+        $name: String!
+        $directory: Boolean!
+        $content: String
+        $app: ID!
+    ) {
+        createSourceFile(
+            name: $name
+            directory: $directory
+            content: $content
+            app: $app
+        ) {
+            id
+        }
+    }
+`;
+
+const EMPTY_SCREEN_HTX_TEMPLATE = `<fragment id="default">
+    <meta>
+        <title>Home | Built with Hypertool</title>
+        <route path="/" weight=999 />
+    </meta>
+    <View id="root">
+        <Button id="counter" text="[PLACEHOLDER TEXT]" />
+    </View>
+</fragment>
+`;
+
+const EMPTY_SCREEN_CSS_TEMPLATE = `#root {
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
 }
+`;
 
-function PlusSquare(props: SvgIconProps) {
-    return (
-        <SvgIcon
-            fontSize="inherit"
-            style={{ width: 14, height: 14 }}
-            {...props}
-        >
-            {/* tslint:disable-next-line: max-line-length */}
-            <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
-        </SvgIcon>
-    );
+const EMPTY_SCREEN_JS_TEMPLATE = `import { useState } from "react";
+
+const Home = () => {
+    const [count, setCount] = useState(0);
+
+    return {
+        counter: {
+            onClick: () => setCount(count => count + 1),
+            text: \`This button was clicked \${count} times.\`
+        }
+    };
+};
+`;
+
+interface INewFile {
+    name: string;
+    type: TNewFileType;
+    parent: IPathNode;
 }
-
-function CloseSquare(props: SvgIconProps) {
-    return (
-        <SvgIcon
-            className="close"
-            fontSize="inherit"
-            style={{ width: 14, height: 14 }}
-            {...props}
-        >
-            {/* tslint:disable-next-line: max-line-length */}
-            <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
-        </SvgIcon>
-    );
-}
-
-function TransitionComponent(props: TransitionProps) {
-    return <Collapse {...props} />;
-}
-
-const root = files.createPathTree([
-    {
-        key: "hello-world/components/button.tsx",
-        directory: false,
-    },
-    {
-        key: "hello-world/components/checkbox.tsx",
-        directory: false,
-    },
-    {
-        key: "hello-world/components/text-field.tsx",
-        directory: false,
-    },
-    {
-        key: "hello-world/screens/index.tsx",
-        directory: false,
-    },
-]);
-
-const StyledTreeItem = styled((props: TreeItemProps) => (
-    <TreeItem {...props} TransitionComponent={TransitionComponent} />
-))(({ theme }) => ({
-    [`& .${treeItemClasses.iconContainer}`]: {
-        "& .close": {
-            opacity: 0.3,
-        },
-    },
-    [`& .${treeItemClasses.group}`]: {
-        marginLeft: 15,
-        paddingLeft: 18,
-        borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
-    },
-}));
 
 const Controllers = () => {
-    const renderNode = (node: any) => (
-        <StyledTreeItem nodeId={Math.random().toString()} label={node.name}>
-            {node.children.map(renderNode)}
-        </StyledTreeItem>
+    const appId = useParam("appId");
+    const { data } = useQuery(GET_SOURCE_FILES, {
+        variables: {
+            page: 0,
+            limit: 20,
+            app: appId,
+        },
+        notifyOnNetworkStatusChange: true,
+    });
+    const { records } = data?.getSourceFiles || { records: [] };
+    const root = useMemo(() => createPathTree(records), [records]);
+
+    const [createSourceFile] = useMutation(CREATE_SOURCE_FILE, {
+        refetchQueries: ["GetSourceFiles"],
+    });
+
+    const handleNew = useCallback(
+        (values: INewFile) => {
+            const { name, type, parent } = values;
+            console.log(values);
+            const create = (
+                name: string,
+                directory: boolean,
+                content: string | null,
+                parent: IPathNode,
+            ) => {
+                return createSourceFile({
+                    variables: {
+                        name: `${parent.path.name}/${name}`,
+                        directory,
+                        content,
+                        app: appId,
+                    },
+                });
+            };
+            switch (type) {
+                case "folder": {
+                    create(name, true, null, parent);
+                    break;
+                }
+
+                case "regular_file": {
+                    create(name, false, null, parent);
+                    break;
+                }
+
+                case "screen": {
+                    // TODO: Move multiple calls operations to the API for atomicity.
+                    create(`/${name}`, true, null, parent);
+                    create(
+                        `/${name}/${name}.htx`,
+                        false,
+                        EMPTY_SCREEN_HTX_TEMPLATE,
+                        parent,
+                    );
+                    create(
+                        `/${name}/${name}.css`,
+                        false,
+                        EMPTY_SCREEN_CSS_TEMPLATE,
+                        parent,
+                    );
+                    create(
+                        `/${name}/${name}.js`,
+                        false,
+                        EMPTY_SCREEN_JS_TEMPLATE,
+                        parent,
+                    );
+                    break;
+                }
+
+                default: {
+                    throw new Error(`Unknown file type "${type}".`);
+                }
+            }
+        },
+        [appId, createSourceFile],
     );
 
     return (
         <TreeView
             defaultExpanded={["1"]}
-            defaultCollapseIcon={<MinusSquare />}
-            defaultExpandIcon={<PlusSquare />}
-            defaultEndIcon={<CloseSquare />}
+            defaultCollapseIcon={<Icon>keyboard_arrow_down</Icon>}
+            defaultExpandIcon={<Icon>chevron_right</Icon>}
+            defaultEndIcon={<Icon>article</Icon>}
             sx={{ height: 264, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
         >
-            {renderNode(root)}
+            {root && <FileTreeNode node={root} onNew={handleNew} />}
         </TreeView>
     );
 };
