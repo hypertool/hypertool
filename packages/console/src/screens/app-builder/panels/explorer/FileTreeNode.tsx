43b1eae73819f142c538e135c4194c5f1d49f9b5
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-23 19:51:02 +0530

    feat: added context menu and new screen dialog to file tree node component

--- packages/console/src/screens/app-builder/panels/explorer/FileTreeNode.tsx
+++ packages/console/src/screens/app-builder/panels/explorer/FileTreeNode.tsx
@@ -7,21 +7,29 @@ import {
     Collapse,
     Divider,
 } from "@mui/material";
+
 import { TreeItem, treeItemClasses } from "@mui/lab";
 import type { TreeItemProps } from "@mui/lab";
 import { alpha, styled } from "@mui/material/styles";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import type { TransitionProps } from "@mui/material/transitions";
-import { IPathNode } from "../../../../types";
-import { useContextMenu } from "../../../../hooks";
+import { IPath, IPathNode, TNewFileType } from "../../../../types";
+import { useBuilderActions, useContextMenu } from "../../../../hooks";
+import { truthy } from "../../../../utils";
+
+import NewScreenDialog from "./NewScreenDialog";
 
 const TransitionComponent = (props: TransitionProps) => {
     return <Collapse {...props} />;
 };
 
-const StyledTreeItem = styled((props: TreeItemProps) => (
+interface IStyledTreeItemProps extends TreeItemProps {
+    contextMenuOpen: boolean;
+}
+
+const StyledTreeItem = styled((props: IStyledTreeItemProps) => (
     <TreeItem {...props} TransitionComponent={TransitionComponent} />
-))(({ theme }) => ({
+))(({ theme, contextMenuOpen }) => ({
     [`& .${treeItemClasses.iconContainer}`]: {
         "& .close": {
             opacity: 0.3,
@@ -32,74 +40,106 @@ const StyledTreeItem = styled((props: TreeItemProps) => (
         paddingLeft: 18,
         borderLeft: `1px dashed ${alpha(theme.palette.text.primary, 0.4)}`,
     },
+    [`& > .${treeItemClasses.content}`]: {
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderColor: contextMenuOpen
+            ? theme.palette.primary.light
+            : "transparent",
+    },
 }));
 
 export interface IFileTreeNodeProps {
     absolutePath: string;
     name: string;
+    onNew: (name: string, type: TNewFileType, parent: IPath) => void;
 }
 
 const FileTreeNode = (props: any) => {
-    const { node } = props;
+    const { node, onNew } = props;
     const { anchor, mouseX, mouseY, onContextMenuOpen, onContextMenuClose } =
         useContextMenu();
+    const actions = useBuilderActions();
+    const [open, setOpen] = useState(false);
+
+    const directory = node.path?.directory || false;
+    const appName = actions.getApp().name;
 
     const menuItems = useMemo(
-        () => [
-            {
-                id: "new_file",
-                text: "New File",
-                icon: "add_circle",
-                callback: () => {
-                    onContextMenuClose();
+        () =>
+            [
+                node.path?.name === `/${appName}/screens` && {
+                    id: "new_screen",
+                    text: "New Screen",
+                    icon: "wysiwyg",
+                    callback: () => {
+                        setOpen(true);
+                        onContextMenuClose();
+                    },
                 },
-            },
-            {
-                id: "new_folder",
-                text: "New Folder",
-                icon: "create_new_folder",
-                callback: () => {
-                    onContextMenuClose();
+                directory && {
+                    id: "new_file",
+                    text: "New File",
+                    icon: "add_circle",
+                    callback: () => {
+                        onContextMenuClose();
+                    },
                 },
-            },
-            {
-                id: "copy_path",
-                text: "Copy Path",
-                icon: "file_copy",
-                callback: () => {
-                    onContextMenuClose();
+                directory && {
+                    id: "new_folder",
+                    text: "New Folder",
+                    icon: "create_new_folder",
+                    callback: () => {
+                        onContextMenuClose();
+                    },
                 },
-            },
-            null,
-            {
-                id: "rename",
-                text: "Rename",
-                icon: "drive_file_rename_outline",
-                callback: () => {
-                    onContextMenuClose();
+                {
+                    id: "copy_path",
+                    text: "Copy Path",
+                    icon: "file_copy",
+                    callback: () => {
+                        onContextMenuClose();
+                    },
                 },
-            },
-            {
-                id: "delete",
-                text: "Delete",
-                icon: "delete",
-                callback: () => {
-                    onContextMenuClose();
+                "<divider>",
+                {
+                    id: "rename",
+                    text: "Rename",
+                    icon: "drive_file_rename_outline",
+                    callback: () => {
+                        onContextMenuClose();
+                    },
                 },
-            },
-        ],
-        [onContextMenuClose],
+                {
+                    id: "delete",
+                    text: "Delete",
+                    icon: "delete",
+                    callback: () => {
+                        onContextMenuClose();
+                    },
+                },
+            ].filter(truthy),
+        [appName, directory, node.path?.name, onContextMenuClose],
     );
 
     return (
-        <StyledTreeItem
-            nodeId={node.name}
-            label={node.name}
-            onContextMenu={onContextMenuOpen}
-        >
-            {node.children.map((child: IPathNode) => (
-                <FileTreeNode node={child} />
-            ))}
+        <>
+            <NewScreenDialog
+                open={open}
+                onCreate={(values: any) =>
+                    onNew({ ...values, type: "screen", parent: node })
+                }
+            />
+            <StyledTreeItem
+                nodeId={node.name}
+                label={node.name}
+                onContextMenu={onContextMenuOpen}
+                contextMenuOpen={Boolean(anchor)}
+            >
+                {node.children.map((child: IPathNode) => (
+                    <FileTreeNode node={child} onNew={onNew} />
+                ))}
+            </StyledTreeItem>
 
             <Menu
                 anchorReference="anchorPosition"
@@ -109,7 +149,7 @@ const FileTreeNode = (props: any) => {
             >
                 {menuItems.map((menuItem) => (
                     <>
-                        {menuItem && (
+                        {menuItem !== "<divider>" && (
                             <MenuItem
                                 id={menuItem.id}
                                 onClick={menuItem.callback}
@@ -122,11 +162,11 @@ const FileTreeNode = (props: any) => {
                                 <ListItemText>{menuItem.text}</ListItemText>
                             </MenuItem>
                         )}
-                        {!menuItem && <Divider />}
+                        {menuItem === "<divider>" && <Divider />}
                     </>
                 ))}
             </Menu>
-        </StyledTreeItem>
+        </>
     );
 };
 
