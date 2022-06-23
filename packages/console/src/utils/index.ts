ddd8ac75991822f39f3c7c5870468aec8179d337
Samuel Rowe <samuelrowe1999@gmail.com>
2022-06-23 19:58:17 +0530

    feat: created `truthy` utility function

--- packages/console/src/utils/index.ts
+++ packages/console/src/utils/index.ts
@@ -2,3 +2,9 @@ export { default as theme } from "./theme";
 export * as templates from "./templates";
 export * as constants from "./constants";
 export * as files from "./files";
+
+export type Truthy<T> = T extends false | "" | 0 | null | undefined ? never : T;
+
+export const truthy = <T>(value: T): value is Truthy<T> => {
+    return !!value;
+};
